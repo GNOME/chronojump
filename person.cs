fb1105742385634966d98c59df4e60f276aa37e8
Xavier de Blas <xaviblas@src.gnome.org>
2004-12-12 12:38:18 +0000

    Corrected ols Gsalta name, show authors, and better showing of GPL

--- person.cs
+++ person.cs
@@ -1,11 +1,12 @@
 /*
+ * This file is part of ChronoJump
  *
- * gsalta is free software; you can redistribute it and/or modify
+ * ChronoJump is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or   
  *    (at your option) any later version.
  *    
- * gsalta is distributed in the hope that it will be useful,
+ * ChronoJump is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
  *    GNU General Public License for more details.
@@ -13,6 +14,9 @@
  * You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Xavier de Blas: 
+ * http://www.xdeblas.com, http://www.deporteyciencia.com (parleblas)
  */
 
 using System;

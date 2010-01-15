023864e3fb48624990118579321f999a0da17708
Xavier de Blas <xaviblas@gmail.com>
2010-01-15 17:22:53 +0100

    server automatically graph new data at night (if there's new data) Print info label to user about this. Added server R graphs to git

---
+++ chronojump_server/r-sqlite/tv_by_level_sex_jumptype.R
@@ -0,0 +1,37 @@
+library(GDD)
+GDD(file="/var/www/web/server/images/tv_by_level_sex_jumptype.png", type="png", w=670, h=670)
+library(RSQLite)
+drv = dbDriver("SQLite")
+file = "/root/.local/share/Chronojump/database/chronojump_server.db"
+con = dbConnect(drv, file)
+
+jumps <- dbGetQuery(con, "select person.sex as sex, person.practice as level, jump.* from person, jump where level>=0 and simulated>=0 and person.uniqueID == jump.personID")
+
+jumps$sexF <- factor(jumps$sex, levels=c('M','F'), ordered=TRUE)
+jumps$typeF <- factor(jumps$type)
+
+library(car)
+jumps$sexNum <- recode(jumps$sexF, '"M" = .1; "F" = -.1; ',
+  as.factor.result=FALSE)
+
+
+  library(lattice)
+  cols=c(topo.colors(4)[1], topo.colors(4)[2])
+     par(pch=19, col=cols, cex.sub=0.75, font.sub=3, col.sub="red")
+  xyplot(tv ~ (level+sexNum) |  typeF, groups=sexF,
+     #simpleTheme don't work in R 2.7.1
+     #par.settings = simpleTheme(pch=19,col=cols),
+     scales=list(x=list(tick.number=3, relation='same'),
+     y=list(relation='same')),
+     xlab="level",
+     pch=19, col=cols,
+     auto.key=list(border=FALSE, text=c("Males", "Females"), col=cols, points = FALSE),
+       #title="TV by level, sex and jump type", sub="hola"),
+       main="TV by level, sex and jump type",
+        sub=paste(Sys.Date(),"(YYYY-MM-DD)"), cex.sub = 0.75, font.sub = 3, col.sub = "red",
+     #key=list(title(main="TV by level, sex and jump type",
+     #   sub=paste(Sys.Date(),"(YYYY-MM-DD)"), cex.sub = 0.75, font.sub = 3, col.sub = "red")),
+     data=jumps)
+
+  dev.off()
+

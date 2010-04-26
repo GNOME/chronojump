b90d93431e4ecf04db96f7a8f0fea3cf436c6e6d
Xavier de Blas <xaviblas@gmail.com>
2010-04-27 01:52:10 +0800

    kneeAngle prediction cleaner

--- src/angle/kneeAngleRInside.cpp
+++ src/angle/kneeAngleRInside.cpp
@@ -25,121 +25,67 @@
 #include <string>
 
 
-CvSeq* predictPointsRInside()
+SEXP ansP;
+std::string txtP = "";
+
+void createPredictFunction()
 {
-	/* TESTING:
+	//don't put comments '#'
+	txtP = "span = 30;	\
+		threshold = 3;	\
+		discard = 10;	\
+				\
+		if( length(x) < discard || sd(x) == 0) {			\
+			n<- x[ length( x ) ];					\
+		} else {							\
+			which.predict <- max( 1, length(x) - span):length(x);	\
+			time.1 <- 1:length( which.predict );			\
+			x.1    <- x[ which.predict ];				\
+										\
+			resid  <- x.1 - predict( lm( x.1 ~ time.1 ), data.frame( time.1 = time.1 ) );	\
+			x.outlier <- abs( resid ) > threshold * sd( resid );	\
+										\
+			time.1   <- time.1[!x.outlier];				\
+			x.1      <- x.1   [!x.outlier];				\
+										\
+			if( is.na( sd(x.1) ) ) {				\
+				n<-  x[ length( x ) ];				\
+			} else {						\
+				n<- round (					\
+				predict( lm( x.1 ~ time.1 ), newdata = data.frame( time.1 = length( time.1 ) + 1 ) ) 	\
+				);						\
+			}							\
+		}		\
+		";
 
-	R.assign(hipXVector, "d1");
-	R.assign(hipYVector, "d2");
-	R.assign(kneeXVector, "d3");
-	R.assign(kneeYVector, "d4");
-	R.assign(toeXVector, "d5");
-	R.assign(toeYVector, "d6");
-	std::string prova =               // now access in R
-		"cat('\nd1=', d1, '\n');"
-		"cat('\nd2=', d2, '\n');"
-		"cat('\nd3=', d3, '\n');"
-		"cat('\nd4=', d4, '\n');"
-		"cat('\nd5=', d5, '\n');"
-		"cat('\nd6=', d6, '\n');";
-	R.parseEvalQ(prova);
-	*/
+	R.assign( txtP, "txtP"); 
+}
 
+int predictDo(std::vector<int> vect) {
+	if( ! vect.empty() ) {
+		R.assign(vect, "x");
+		R.parseEval(txtP, ansP);
+		return(Rcpp::as< int >(ansP));
+	} else return 0;
+}
+
+CvSeq* predictPoints() {
+	if(txtP=="") 
+		createPredictFunction();
+	
 	CvMemStorage* storage = cvCreateMemStorage(0);
 	CvSeq* seqPoints = cvCreateSeq( CV_SEQ_KIND_GENERIC|CV_32SC2, sizeof(CvSeq), sizeof(CvPoint), storage );
 	CvPoint hip;
-	hip.x =0; hip.y=0;
 	CvPoint knee;
-	knee.x =0; knee.y=0;
 	CvPoint toe;
-	toe.x =0; toe.y=0;
-
-	SEXP ans;
-	std::string txt = "			\
-		span = 30;			\
-		threshold = 3;			\
-		discard = 10;			\
-						\
-		maxX = 512;			\
-		maxY = 384;			\
-		#cat('(', length(x), '): ', x[length(x)]);		\
-												\
-		if( length(x) < discard || sd(x) == 0) {					\
-			n<- x[ length( x ) ];							\
-		} else {									\
-			which.predict <- max( 1, length(x) - span):length(x);			\
-			time.1 <- 1:length( which.predict );					\
-			x.1    <- x[ which.predict ];						\
-												\
-			resid  <- x.1 - predict( lm( x.1 ~ time.1 ), data.frame( time.1 = time.1 ) );	\
-			x.outlier <- abs( resid ) > threshold * sd( resid );			\
-												\
-			time.1   <- time.1[!x.outlier];						\
-			x.1      <- x.1   [!x.outlier];						\
-												\
-			if( is.na( sd(x.1) ) ) {						\
-				n<-  x[ length( x ) ];						\
-			} else {								\
-				n<- round (							\
-				predict( lm( x.1 ~ time.1 ), newdata = data.frame( time.1 = length( time.1 ) + 1 ) ) 	\
-				);								\
-			}									\
-		}				\
-		";
-
-	R.assign( txt, "txt"); 
 	
-	//HIP
-	if(hipXVector.empty())
-		hip.x = -1;
-	else {
-		R.assign(hipXVector, "x");
-		R.parseEval(txt, ans);
-		hip.x = Rcpp::as< int >(ans);
-	}
+	hip.x =  predictDo(hipXVector);
+	hip.y =  predictDo(hipYVector);
+	knee.x = predictDo(kneeXVector);
+	knee.y = predictDo(kneeYVector);
+	toe.x =  predictDo(toeXVector);
+	toe.y =  predictDo(toeYVector);
 	
-	if(hipYVector.empty())
-		hip.y = -1;
-	else {
-		R.assign(hipYVector, "x");
-		R.parseEval(txt, ans);
-		hip.y = Rcpp::as< int >(ans);
-	}
-
-	//KNEE
-	if(kneeXVector.empty())
-		knee.x = -1;
-	else {
-		R.assign(kneeXVector, "x");
-		R.parseEval(txt, ans);
-		knee.x = Rcpp::as< int >(ans);
-	}
-
-	if(kneeYVector.empty())
-		knee.y = -1;
-	else {
-		R.assign(kneeYVector, "x");
-		R.parseEval(txt, ans);
-		knee.y = Rcpp::as< int >(ans);
-	}
-
-	//TOE
-	if(toeXVector.empty())
-		toe.x = -1;
-	else {
-		R.assign(toeXVector, "x");
-		R.parseEval(txt, ans);
-		toe.x = Rcpp::as< int >(ans);
-	}
-
-	if(toeYVector.empty())
-		toe.y = -1;
-	else {
-		R.assign(toeYVector, "x");
-		R.parseEval(txt, ans);
-		toe.y = Rcpp::as< int >(ans);
-	}
-		
 	//PASS to cvSeq	
 	cvSeqPush( seqPoints, &hip );
 	cvSeqPush( seqPoints, &knee );

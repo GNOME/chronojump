fc7ad42046f69db0e587a2a5c9098cd019e0d33d
Xavier de Blas <xaviblas@gmail.com>
2011-06-03 13:55:58 +0200

    Added 2 graphs to server

--- chronojump_server/r-sqlite/evaluators.R
+++ chronojump_server/r-sqlite/evaluators.R
@@ -7,14 +7,29 @@ con = dbConnect(drv, file)
 
 jumps <- dbGetQuery(con, "SELECT COUNT(jump.uniqueID) AS conta, SEvaluator.name AS names FROM jump, SEvaluator, session WHERE jump.sessionID=session.uniqueID AND session.evaluatorID=Sevaluator.uniqueID GROUP BY SEvaluator.name ORDER BY SEvaluator.name;")
 
+jumpsRj <- dbGetQuery(con, "SELECT COUNT(jumpRj.uniqueID) AS conta, SEvaluator.name AS names FROM jumpRj, SEvaluator, session WHERE jumpRj.sessionID=session.uniqueID AND session.evaluatorID=Sevaluator.uniqueID GROUP BY SEvaluator.name ORDER BY conta DESC;")
+
 persons <- dbGetQuery(con, "SELECT COUNT(DISTINCT(person77.uniqueID)) AS conta, SEvaluator.name AS names FROM person77, SEvaluator, session, personSession77 WHERE person77.uniqueID=personSession77.personID AND session.uniqueID=personSession77.sessionID AND session.evaluatorID=Sevaluator.uniqueID GROUP BY SEvaluator.name ORDER BY SEvaluator.name;")
 
-colors=c(topo.colors(8)[3],topo.colors(8)[6])
-o.j.d <- order(jumps$conta, decreasing=T)
+def.par <- par(no.readonly = TRUE) # save default, for resetting...
+par(new=FALSE, oma=c(1,1,5,1))
+#nf <- layout(matrix(c(1,1,2,3), 2, 2, byrow=TRUE), respect=TRUE)
+par(mfcol=c(3,1))
 
-barplot(rbind(jumps$conta[o.j.d], persons$conta[o.j.d]), beside=T, legend=c("Jumps","Persons"), names=jumps$names[o.j.d], col=colors)
+persons$names<-factor(persons$names)
+jumps$names<-factor(jumps$names)
+jumpsRj$names<-factor(jumpsRj$names)
 
-title(main="Jumps and persons count by evaluator",
+cex=.7
+dotchart(persons$conta[order (persons$conta)], labels=levels(persons$names)[order (persons$conta)], main="Persons", cex=cex)
+dotchart(jumps$conta[order (jumps$conta)], labels=levels(jumps$names)[order (jumps$conta)], main="Jumps (simple)", cex=cex)
+dotchart(jumpsRj$conta[order (jumpsRj$conta)], labels=levels(jumpsRj$names)[order (jumpsRj$conta)], main="Jumps (reactive)", cex=cex)
+
+
+par(def.par)#- reset to default
+par(new=TRUE)
+plot(-1,type="n",axes=F,xlab='',ylab='')
+title(main="Data uploaded by evaluator",
   sub=paste(Sys.Date(),"(YYYY-MM-DD)"), cex.sub = 0.75, font.sub = 3, col.sub = "red")
 
 dev.off()
---
+++ chronojump_server/r-sqlite/ieiub.R
@@ -0,0 +1,62 @@
+library(GDD)
+GDD(file="/var/www/web/server/images/ieiub.png", type="png", w=670, h=670)
+
+library(RSQLite)
+drv = dbDriver("SQLite")
+file = "/root/.local/share/Chronojump/database/chronojump_server.db"
+con = dbConnect(drv, file)
+
+def.par <- par(no.readonly = TRUE) # save default, for resetting...
+
+IE <- dbGetQuery(con, "SELECT person77.name, person77.sex AS sex, personSession77.practice AS level,  ( AVG(j1.tv) - AVG(j2.tv) )*100/(AVG(j2.tv)*1.0) AS myIndex, AVG(j1.tv), AVG(j2.tv) FROM jump AS j1, jump AS j2, person77, personSession77 WHERE j1.sessionID == j2.sessionID AND j1.type == 'CMJ'  AND j2.type == 'SJ'  AND j1.personID == person77.uniqueID  AND j2.personID == person77.uniqueID AND person77.UniqueID == personSession77.PersonID GROUP BY j1.personID, j1.sessionID ORDER BY  myIndex DESC")
+IUB <- dbGetQuery(con, "SELECT person77.name, person77.sex AS sex, personSession77.practice AS level,  ( AVG(j1.tv) - AVG(j2.tv) )*100/(AVG(j2.tv)*1.0) AS myIndex, AVG(j1.tv), AVG(j2.tv) FROM jump AS j1, jump AS j2, person77, personSession77 WHERE j1.sessionID == j2.sessionID AND j1.type == 'ABK'  AND j2.type == 'CMJ'  AND j1.personID == person77.uniqueID  AND j2.personID == person77.uniqueID AND person77.UniqueID == personSession77.PersonID GROUP BY j1.personID, j1.sessionID ORDER BY  myIndex DESC")
+IEm <- subset (IE, IE$sex == 'M')
+IEf <- subset (IE, IE$sex == 'F')
+IUBm <- subset (IUB, IUB$sex == 'M')
+IUBf <- subset (IUB, IUB$sex == 'F')
+
+par(new=FALSE, oma=c(1,1,5,1))
+par(mfcol=c(2,2))
+levels=c("sed.", "regul.", "compet.", "elite")
+
+bp = boxplot(IEm$myIndex~IEm$level, main="IE Man by practice level",  axes=F)
+text(1:4, -5, paste("n=",format(bp$n),sep=""), xpd = TRUE)
+mtext("IE=100*(CMJ-SJ)/SJ", cex=.7)
+axis(1, at = 1:4, labels = levels)
+axis(2)
+box()
+abline(h=0)
+
+bp = boxplot(IUBm$myIndex~IUBm$level, main="IUB Man by practice level",  axes=F)
+text(1:4, -5, paste("n=",format(bp$n),sep=""), xpd = TRUE)
+mtext("IUB=100*(ABK-CMJ)/CMJ",cex=.7)
+axis(1, at = 1:4, labels = levels)
+axis(2)
+box()
+abline(h=0)
+
+bp = boxplot(IEf$myIndex~IEf$level, main="IE Woman by practice level", axes=F)
+text(1:4, -5, paste("n=",format(bp$n),sep=""), xpd = TRUE)
+mtext("IE=100*(CMJ-SJ)/SJ",cex=.7)
+axis(1, at = 1:4, labels = levels)
+axis(2)
+box()
+abline(h=0)
+
+bp = boxplot(IUBf$myIndex~IUBf$level, main="IUB Woman by practice level", axes=F)
+text(1:4, -5, paste("n=",format(bp$n),sep=""), xpd = TRUE)
+mtext("IUB=100*(ABK-CMJ)/CMJ",cex=.7)
+axis(1, at = 1:4, labels = levels)
+axis(2)
+box()
+abline(h=0)
+
+
+par(def.par)#- reset to default
+
+par(new=TRUE)
+plot(-1,type="n",axes=F,xlab='',ylab='')
+title(main="Elasticity Index and Using Arms Index",
+  sub=paste("Values are AVG of both jumps of each person on every session. Date of graph: ",Sys.Date(),"(YYYY-MM-DD)"), cex.sub = 0.75, font.sub = 3, col.sub = "red")
+
+dev.off()

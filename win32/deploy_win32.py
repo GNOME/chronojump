6cbc77660293f9416f3667264e826e09719ab2af
Andoni Morales Alastruey <ylatuya@gmail.com>
2013-01-27 15:38:08 +0100

    Deploy gtk+ themes correctly

--- win32/deploy_win32.py
+++ win32/deploy_win32.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abd17f2d9d7c983237a1ef43b069500666ae170c6145fae8fb1ccd6acc8a46f5
-size 10242
+oid sha256:5f5042ddb7bd5d5b3be451c87cbe768f1f1e90d915e91c1a4257e1860a5cf261
+size 10191

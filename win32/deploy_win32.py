029cc16feb6e0b8f61c5b64fb2945dbad5c711dc
Xavier de Blas <xaviblas@gmail.com>
2013-04-29 21:40:34 +0200

    Add missing libgstapp and libffi-6

--- win32/deploy_win32.py
+++ win32/deploy_win32.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f5042ddb7bd5d5b3be451c87cbe768f1f1e90d915e91c1a4257e1860a5cf261
-size 10191
+oid sha256:851a7c28aa9b31592f1acdbfa6f81cbf442d0ff694dfc4c9560fbb583828b5ae
+size 10208

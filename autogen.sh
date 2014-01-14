fac25cd33ef020d4597a4ac8a76ad13534314398
Andoni Morales Alastruey <ylatuya@gmail.com>
2014-01-14 16:14:51 +0100

    Improve autogen.sh script

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,27 @@
-#! /bin/sh
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
 
-autoreconf -ivf
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd "$srcdir"
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+	echo "*** No autoreconf found, please install it ***"
+	exit 1
+else
+	ACLOCAL="aclocal $ACLOCAL_FLAGS" autoreconf --force --install || exit $?
+fi
+
+INTLTOOLIZE=`which intltoolize`
+if test -z $INTLTOOLIZE; then
+	echo "*** No intltoolize found, please install it ***"
+	exit 1
+else
+	intltoolize || exit $?
+fi
+
+cd "$olddir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"

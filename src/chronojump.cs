97fc51631e05a89ad12492f5a739c833763313dc
Xavier de Blas <xaviblas@gmail.com>
2014-04-08 14:33:51 +0200

    CSV support 25% (read file works)

--- src/chronojump.cs
+++ src/chronojump.cs
@@ -133,7 +133,8 @@ public class ChronoJump
 		Log.WriteLine("baseDir1:" + baseDirectory);
 		Log.WriteLine("envPath+rBinPath:" + envPath + Path.PathSeparator + rBinPath);
 		
-		
+	
+		//UtilCSV.ReadValues("/tmp/chronojump-encoder-graph-input-multi.csv");	
 		
 		if(UtilAll.IsWindows())
 			Environment.SetEnvironmentVariable("GST_PLUGIN_PATH",RelativeToPrefix("lib\\gstreamer-0.10"));

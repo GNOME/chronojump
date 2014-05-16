0eba70dbb52721995e64c86059cd3d7a2dbb45bc
Xavier de Blas <xaviblas@gmail.com>
2014-05-16 18:38:06 +0200

    Shown accuracy warning message on conical machines

--- src/gui/encoderConfiguration.cs
+++ src/gui/encoderConfiguration.cs
@@ -222,6 +222,12 @@ public class EncoderConfigurationWindow {
 		return ec;
 	}
 	
+	void on_button_encoder_capture_inertial_accuracy_clicked (object o, EventArgs args) {
+		new DialogMessage(Constants.MessageTypes.WARNING, 
+				Catalog.GetString("Calculation of dynamic variables like power in conical machines is not very accurate because current method is not using the variation of the cone diameter as a variable.") + "\n\n" +
+				Catalog.GetString("Future versions will include a better way to calcule this. Sorry for the inconvenience."));
+	}
+	
 	bool show_calcule_im = false;
 	void on_button_encoder_capture_inertial_show_clicked (object o, EventArgs args) 
 	{

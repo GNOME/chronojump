92dba46d478d99084d69c928089e2704dfd27ed2
Xavier de Blas <xaviblas@gmail.com>
2015-01-14 12:17:13 +0100

    Better utilDate (don't need try/catch)

--- src/utilDate.cs
+++ src/utilDate.cs
@@ -51,41 +51,40 @@ public class UtilDate
 	//return datetime
 	public static DateTime FromSql (string date)
 	{
-		try {
-			/*
-			   maybe date format is before 0.72 (d/m/Y)
-			   this is still here and not in a standalone conversion
-			   because if someone converts from older database
-			   can have problems wih constructors with different date formats
-			 */
-
-			DateTime dt; //Datetime (year, month, day) constructor
-			if(date.IndexOf('/') == -1) { 
-				//not contains '/'
-				//new sqlite3 compatible date format sice db 0.72 YYYY-MM-DD
-				string [] dateFull = date.Split(new char[] {'-'});
-				dt = new DateTime(
-						Convert.ToInt32(dateFull[0]), 
-						Convert.ToInt32(dateFull[1]), 
-						Convert.ToInt32(dateFull[2]));
-			} else {
-				//contains '/'
-				//old D/M/Y format 
-				string [] dateFull = date.Split(new char[] {'/'});
-				dt = new DateTime(
-						Convert.ToInt32(dateFull[2]), 
-						Convert.ToInt32(dateFull[1]), 
-						Convert.ToInt32(dateFull[0]));
-			}
-			return dt;
-		}
-		catch {
-			/*
-			   on report we do a session select with uniqueID = -1
-			   it returns nothing, date has nothing
-			   */
+		/*
+		   on report we do a session select with uniqueID = -1
+		   it returns nothing, date has nothing
+		   */
+		if(date == null || date == "")
 			return DateTime.Now;
+
+
+		/*
+		   maybe date format is before 0.72 (d/m/Y)
+		   this is still here and not in a standalone conversion
+		   because if someone converts from older database
+		   can have problems wih constructors with different date formats
+		   */
+
+		DateTime dt; //Datetime (year, month, day) constructor
+		if(date.IndexOf('/') == -1) { 
+			//not contains '/'
+			//new sqlite3 compatible date format sice db 0.72 YYYY-MM-DD
+			string [] dateFull = date.Split(new char[] {'-'});
+			dt = new DateTime(
+					Convert.ToInt32(dateFull[0]), 
+					Convert.ToInt32(dateFull[1]), 
+					Convert.ToInt32(dateFull[2]));
+		} else {
+			//contains '/'
+			//old D/M/Y format 
+			string [] dateFull = date.Split(new char[] {'/'});
+			dt = new DateTime(
+					Convert.ToInt32(dateFull[2]), 
+					Convert.ToInt32(dateFull[1]), 
+					Convert.ToInt32(dateFull[0]));
 		}
+		return dt;
 	}
 
 }

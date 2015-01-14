dc73b5ea0c49e4b9649a46e22e01b3b06cd56fec
Xavier de Blas <xaviblas@gmail.com>
2015-01-14 15:13:17 +0100

    Trying a no-rdotnet way to communicate with R sequentially

---
+++ no-rdotnet/passToR.R
@@ -0,0 +1,10 @@
+#http://stackoverflow.com/questions/26053302/is-there-a-way-to-use-standard-input-output-stream-to-communicate-c-sharp-and-r/26058010#26058010
+f <- file("stdin")
+open(f)
+
+input <- readLines(f, n = 1L)
+while(input[1] != "Q") {
+	cat(paste("input is:", input, "\n"))
+	input <- readLines(f, n = 1L)
+}
+quit()

fa6a011d2e201a17645c50a881b671885d208ed2
Xavier de Blas <xaviblas@gmail.com>
2015-01-22 00:06:26 +0100

    Encoder capture data == graph.R data

--- encoder/capture.R
+++ encoder/capture.R
@@ -20,15 +20,12 @@ getOptionsFromFile <- function(optionsFile, lines) {
 	return (options)
 }
 
-#options <- getOptionsFromFile(optionsFile, 1)
 options <- getOptionsFromFile(optionsFile, 32)
 
 
-#scriptUtilR = options[1]
 scriptUtilR = options[28]
 source(scriptUtilR)
 
-#print("Loaded libraries")
 	
 input <- readLines(f, n = 1L)
 while(input[1] != "Q") {
@@ -54,33 +51,16 @@ while(input[1] != "Q") {
 		displacement = getDisplacement(op$EncoderConfigurationName, displacement, op$diameter, op$diameterExt)
 	}
 	
-	write(displacement,stderr())
 	position = cumsum(displacement)
-	write(position,stderr())
-		
-	#curves=findCurves(displacement, op$Eccon, op$MinHeight, FALSE, op$Title) #draw: FALSE
-	#print("curves",stderr())
-	#print(curves,stderr())
-				
+
 	if( ! isInertial(op$EncoderConfigurationName)) {
 		reduceTemp = reduceCurveBySpeed(op$Eccon, 1, 
-						#curves[1,1], curves[1,3], #startT, startH
 						1, 0, #startT, startH
 						displacement, #displacement
 						op$SmoothingOneC #SmoothingOneC
 						)
-				
-		#curves[1,1] = reduceTemp[1]
-		#curves[1,2] = reduceTemp[2]
-		#curves[1,3] = reduceTemp[3]
 	}
 		
-#	SmoothingsEC = findSmoothingsEC(singleFile, displacement, curves, op$Eccon, op$SmoothingOneC)
-#	write(c("SmoothingsEC:",SmoothingsEC),stderr())
-	
-	write("curves after reduceCurveBySpeed",stderr())
-	#write(as.vector(curves[1,]),stderr())
-
 	start = reduceTemp[1]
 	end = reduceTemp[2]
 	#write("printing reduceTemp2", stderr())
@@ -88,16 +68,8 @@ while(input[1] != "Q") {
 	if(end > length(displacement))
 		end = length(displacement)
 	
-	#write("printing reduceTemp", stderr())
-	#write(c(start, end, length(displacement)),stderr())
+	displacement = displacement[start:end]
 
-	#displacement = displacement[start:end]
-
-	#print("reduced")
-
-	#if(curves[1,2] > length(displacement)) 
-	#	curves[1,2] = length(displacement)
-				
 	g = 9.81
 		    
 	#read AnalysisOptions
@@ -110,28 +82,18 @@ while(input[1] != "Q") {
 	#simplify on capture and have the SmoothingEC == SmoothingC
 	SmoothingsEC = op$SmoothingOneC
 
-	#print("pre kinematicsResult")
-
-
-	#kinematicsResult <- kinematicsF(displacement[curves[1,1]:curves[1,2]], 
 	kinematicsResult <- kinematicsF(displacement, 
 		    op$MassBody, op$MassExtra, op$ExercisePercentBodyWeight,
 		    op$EncoderConfigurationName, op$diameter, op$diameterExt, op$anglePush, op$angleWeight, op$inertiaMomentum, op$gearedDown,
-		    #SmoothingsEC[1], op$SmoothingOneC, 
 		    SmoothingsEC, op$SmoothingOneC, 
 		    g, op$Eccon, isPropulsive)
-	#print("kinematicsResult")
-	#print(kinematicsResult)
 
 	paf = data.frame()
 	paf = pafGenerate(op$Eccon, kinematicsResult, op$MassBody, op$MassExtra)
-	#write("paf",stderr())
-	#write(paf,stderr())
 
 	#do not use print because it shows the [1] first. Use cat:
 	cat(paste(#start, #start is not used because we have no data of the initial zeros
 		  (end-start), (position[end]-position[start]),
-		  #curves[1,2]-curves[1,1], position[curves[1,2]]-curves[1,3],
 		  paf$meanSpeed, paf$maxSpeed, paf$maxSpeedT, paf$meanPower, paf$peakPower, paf$peakPowerT, paf$pp_ppt, sep=", "))
 	cat("\n") #mandatory to read this from C#, but beware, there we will need a trim to remove the windows \r\n
 

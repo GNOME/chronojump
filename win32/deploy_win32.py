99e6934c103939d9eb9fbe14bb2ddd271c975256
Windows Build <windows@chronojump.org>
2015-03-09 00:10:43 +0100

    Update build system to the new mono

--- win32/deploy_win32.py
+++ win32/deploy_win32.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8caeca5d4b0777ec0649dd005d21e4d386dd20bca8957d9fed21acdb5ffa54e
-size 10420
+oid sha256:2449af7174c40ed45068c5aa81687f88df2d3fa170979a2a548105870acbad63
+size 10436
--- win32/deps/etc/mono/2.0/DefaultWsdlHelpGenerator.aspx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cdc2077ae31be0ca52997dd9f5dbb9161ed02329901578ffb88b20c9cc055129
-size 59909

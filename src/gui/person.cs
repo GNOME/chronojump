175b0cac8d8209530b1b54afe1bc660dad5c4012
Xavier de Blas <xaviblas@gmail.com>
2015-06-16 20:55:50 +0200

    Person select window 80% done

--- src/gui/person.cs
+++ src/gui/person.cs
@@ -2429,4 +2429,124 @@ public class PersonShowAllEventsWindow {
 	}
 }
 
+public class PersonSelectWindow 
+{
+	[Widget] Gtk.Window person_select_window;
+	[Widget] Gtk.Table table1;
+	
+	static PersonSelectWindow PersonSelectWindowBox;
+	Gtk.Window parent;
+	
+	PersonSelectWindow (Gtk.Window parent) {
+		Glade.XML gladeXML;
+		gladeXML = Glade.XML.FromAssembly (Util.GetGladePath() + "chronojump.glade", "person_select_window", "chronojump");
+		gladeXML.Autoconnect(this);
+		
+		//put an icon to window
+		UtilGtk.IconWindow(person_select_window);
+	}
+	
+	static public PersonSelectWindow Show (Gtk.Window parent, ArrayList persons)
+	{
+		if (PersonSelectWindowBox == null) {
+			PersonSelectWindowBox = new PersonSelectWindow (parent);
+		}
+		
+		PersonSelectWindowBox.parent = parent;
+		//PersonSelectWindowBox.persons = persons;
+		
+		PersonSelectWindowBox.createTable(persons);
+		
+		PersonSelectWindowBox.person_select_window.Show ();
+		
+		return PersonSelectWindowBox;
+	}
+
+	private void createTable(ArrayList persons) 
+	{
+		uint padding = 8;	
+		uint cols = 4; //each row has 4 columns
+		uint rows = Convert.ToUInt32(Math.Floor(persons.Count / (1.0 * cols) ) +1);
+		int count = 0;
+		
+		for (int row_i = 0; row_i < rows; row_i ++) {
+			for (int col_i = 0; col_i < cols; col_i ++) {
+				//Gtk.Label myLabel = new Gtk.Label( row_i.ToString() + "." + col_i.ToString() );
+				if(count >= persons.Count)
+					return;
+				
+				Person p = (Person) persons[count ++];
+
+				Gtk.VBox vbox = new Gtk.VBox();
+			
+				Gtk.Image image = new Gtk.Image();
+				string photoFile = Util.GetPhotoFileName(true, p.UniqueID);
+				if(photoFile != "" && File.Exists(photoFile)) {
+					try {
+						Pixbuf pixbuf = new Pixbuf (photoFile); //from a file
+						image.Pixbuf = pixbuf;
+						image.Visible = true;
+					}
+					catch {
+						LogB.Warning("catched while adding photo");
+					}
+				}
+
+				Gtk.Label label_id = new Gtk.Label(p.UniqueID.ToString());
+				label_id.Visible = false; //hide this to the user
+
+				Gtk.Label label_name = new Gtk.Label(p.Name);
+				label_name.Visible = true;
+
+				vbox.PackStart(image);
+				vbox.PackStart(label_id);
+				vbox.PackStart(label_name);
+
+				vbox.Show();
+				
+				Button b = new Button(vbox);
+				b.Clicked += new EventHandler(on_button_clicked);
+				b.Show();
+				
+				table1.Attach (b, (uint) col_i, (uint) col_i +1, (uint) row_i, (uint) row_i +1, 
+						Gtk.AttachOptions.Fill | Gtk.AttachOptions.Expand, 
+						Gtk.AttachOptions.Fill | Gtk.AttachOptions.Expand, 
+						padding, padding);
+			}
+		}
+	}
+	
+	private void on_button_clicked (object o, EventArgs args)
+	{
+		LogB.Information("Clicked");
+
+		//access the button
+		Button b = (Button) o;
+		
+		//access the vbox
+		Gtk.VBox box = (Gtk.VBox) b.Child;
+		
+		//access the memebers of vbox
+		Array box_elements = box.Children;
+	
+		//access uniqueID	
+		Gtk.Label l = (Gtk.Label) box_elements.GetValue(1); //the ID
+		LogB.Information("UniqueID: " + l.Text.ToString());
+		
+		//access name
+		l = (Gtk.Label) box_elements.GetValue(2); //the name
+		LogB.Information("Name: " + l.Text.ToString());
+
+		//TODO: now need to process the signal and close
+	}
+
+	//TODO: allow to close with ESC
+	
+	private void on_delete_event (object o, DeleteEventArgs args)
+	{
+		PersonSelectWindowBox.person_select_window.Hide();
+		PersonSelectWindowBox = null;
+	}
+}
+
 

dcf09008d796b74b0559dc06476d18e4ef00b169
Xavier de Blas <xaviblas@gmail.com>
2016-01-26 18:45:14 +0100

    Initial code for sliding horizontally over image

--- src/gui/chronojump.cs
+++ src/gui/chronojump.cs
@@ -64,6 +64,9 @@ public partial class ChronoJumpWindow
 	[Widget] Gtk.Image image_selector_start_jumps;
 	[Widget] Gtk.Image image_selector_start_runs;
 	[Widget] Gtk.Image image_selector_start_encoder;
+	
+	[Widget] Gtk.DrawingArea drawingarea_cairo_prova;
+	[Widget] Gtk.HScale hscale_cairo_prova;
 
 	//gui for small screens
 	[Widget] Gtk.RadioButton radio_mode_jumps_small;
@@ -464,6 +467,38 @@ public partial class ChronoJumpWindow
 	//int chronopicCancelledTimes = 0;
 
 
+	Pixbuf cairo_pixbuf;
+	public void on_drawingarea_cairo_prova_expose_event(object o, ExposeEventArgs args)
+	{
+		DrawingArea area = (DrawingArea) o;
+		using (Cairo.Context g = Gdk.CairoHelper.Create (area.GdkWindow)) 
+		{
+			//add image
+			Gdk.CairoHelper.SetSourcePixbuf (g, cairo_pixbuf, 0, 0);
+			g.Paint();
+
+			//add rectangle
+			g.SetSourceRGBA(1, 0, 0, 1); //red
+			
+			int xpos = Convert.ToInt32(hscale_cairo_prova.Value);
+			g.MoveTo(xpos, 0);
+			g.LineTo(xpos, cairo_pixbuf.Height);
+			
+			g.Stroke();
+
+			g.GetTarget ().Dispose ();
+		}
+	}
+	public void on_drawingarea_cairo_prova_configure_event(object o, ConfigureEventArgs args)
+	{
+	}
+	
+	public void on_hscale_cairo_prova_value_changed(object o, EventArgs args)
+	{
+		LogB.Information("changed!");
+		LogB.Information(hscale_cairo_prova.Value.ToString());
+		drawingarea_cairo_prova.QueueDraw(); //will fire ExposeEvent
+	}
 
 	//only called the first time the software runs
 	//and only on windows
@@ -503,6 +538,9 @@ public partial class ChronoJumpWindow
 		this.progVersion = progVersion;
 		this.progName = progName;
 		this.runningFileName = runningFileName;
+		
+		cairo_pixbuf = new Pixbuf (null, Util.GetImagePath(false) + Constants.FileNameLogo);
+		
 
 		Glade.XML gxml;
 		gxml = Glade.XML.FromAssembly (Util.GetGladePath() + "chronojump.glade", "app1", "chronojump");
@@ -837,6 +875,7 @@ public partial class ChronoJumpWindow
 		image_logo.Pixbuf = pixbuf;
 		pixbuf = new Pixbuf (null, Util.GetImagePath(false) + Constants.FileNameLogo320); //changed to 270 for the presentation
 		image_presentation_logo.Pixbuf = pixbuf;
+
 	}
 
 	private void loadPreferences () 

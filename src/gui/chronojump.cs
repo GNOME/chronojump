4c4e53254b4c9cfc1c3f508f111895beb35865b5
Xavier de Blas <xaviblas@gmail.com>
2016-05-27 09:36:42 +0200

    extra mass / diameters depending on gravi/inertial

--- src/gui/chronojump.cs
+++ src/gui/chronojump.cs
@@ -3061,6 +3061,7 @@ public partial class ChronoJumpWindow
 				
 				currentEncoderGI = Constants.EncoderGI.GRAVITATORY;
 				hbox_capture_1RM.Visible = true;
+				notebook_encoder_capture_extra_mass.CurrentPage = 0;
 				if(radio_encoder_analyze_individual_current_set.Active || radio_encoder_analyze_individual_current_session.Active) {
 					radiobutton_encoder_analyze_1RM.Visible = true;
 					radiobutton_encoder_analyze_neuromuscular_profile.Visible = true;
@@ -3077,6 +3078,7 @@ public partial class ChronoJumpWindow
 				
 				currentEncoderGI = Constants.EncoderGI.INERTIAL;
 				hbox_capture_1RM.Visible = false;
+				notebook_encoder_capture_extra_mass.CurrentPage = 1;
 				radiobutton_encoder_analyze_1RM.Visible = false;
 				radiobutton_encoder_analyze_neuromuscular_profile.Visible = false;
 			}

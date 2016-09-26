297341b5ed33a3bc6b402adab58d3150596f556b
Carles Pina i Estany <carles@pina.cat>
2016-09-27 00:52:52 +0200

    Now py2exec will find the chronojump_importer.py file.

--- src/chronojump-importer/py2exec_chronojump_importer.py
+++ src/chronojump-importer/py2exec_chronojump_importer.py
@@ -1,4 +1,8 @@
 from distutils.core import setup
 import py2exe
+import os
 
-setup(console=['chronojump_importer.py'])
+directory=os.path.dirname(os.path.abspath(__file__))
+chronojump_importer_path=os.path.join(directory, "chronojump_importer.py")
+
+setup(console=[chronojump_importer_path])

78b09bc912f5c343b457b43996ddbd91387b1abd
Xavier de Blas <xaviblas@gmail.com>
2016-12-09 13:14:28 +0100

    Fixed unable to delete some encoder curves

--- src/gui/encoder.cs
+++ src/gui/encoder.cs
@@ -1133,7 +1133,6 @@ public partial class ChronoJumpWindow
 			
 	void on_delete_encoder_curve (object o, EventArgs args)
 	{
-		encSelReps.FakeButtonDeleteCurve.Clicked -= new EventHandler(on_delete_encoder_curve);
 		LogB.Information("at on_delete_encoder_curve");
 		delete_encoder_curve(false, encSelReps.DeleteCurveID);
 	}	

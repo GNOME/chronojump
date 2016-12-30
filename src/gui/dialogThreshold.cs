7bc43e4b768f6611c00b391c52d1723318f1751e
Xavier de Blas <xaviblas@gmail.com>
2016-12-30 20:31:02 +0100

    Fixed: 776621 Text trimmed in Threshold window. labels -> textviews

--- src/gui/dialogThreshold.cs
+++ src/gui/dialogThreshold.cs
@@ -22,11 +22,16 @@ using System;
 using Gtk;
 using Gdk;
 using Glade;
+using Mono.Unix;
 
 public class DialogThreshold
 {
 	[Widget] Gtk.Dialog dialog_threshold;
 	[Widget] Gtk.Notebook notebook;
+	[Widget] Gtk.TextView textview_about;
+	[Widget] Gtk.TextView textview_jumps;
+	[Widget] Gtk.TextView textview_races;
+	[Widget] Gtk.TextView textview_other;
 
 	public DialogThreshold (Constants.Menuitem_modes m)
 	{
@@ -37,12 +42,46 @@ public class DialogThreshold
 		//put an icon to window
 		UtilGtk.IconWindow(dialog_threshold);
 
+		writeTexts();
+
 		if(m == Constants.Menuitem_modes.JUMPSSIMPLE || m == Constants.Menuitem_modes.JUMPSREACTIVE)
 			notebook.CurrentPage = 0;
                 else if(m == Constants.Menuitem_modes.RUNSSIMPLE || m == Constants.Menuitem_modes.RUNSINTERVALLIC)
 			notebook.CurrentPage = 1;
 		else 	//other
 			notebook.CurrentPage = 2;
+	}
+
+	private void writeTexts()
+	{
+		TextBuffer tb_about = new TextBuffer (new TextTagTable());
+		tb_about.Text =  Catalog.GetString("Spurius signals are common on electronics.") +
+			"\n\n" + Catalog.GetString("Threshold refers to the minimum value measurable and is the common way to clean this spurius signals.") +
+			"\n"   + Catalog.GetString("Threshold should be a value lower than expected values.") +
+
+			"\n\n" + Catalog.GetString("On database three different thresholds are stored: jumps, races and other tests.") +
+			"\n"  +  Catalog.GetString("If you change this values they will be stored once test is executed.") +
+
+			"\n\n" + Catalog.GetString("Usually threshold values should not be changed but this option is useful for special cases.");
+		textview_about.Buffer = tb_about;
+
+		TextBuffer tb_jumps = new TextBuffer (new TextTagTable());
+		tb_jumps.Text =  Catalog.GetString("Default value: 50 ms.") +
+			"\n\n" + Catalog.GetString("On jumps with contact platforms a value of 50 ms (3 cm jump approximately) is enough to solve electronical problems.") +
+			"\n\n" + Catalog.GetString("You may change this value if you have a jumper that looses pressure with the platform while going down on the eccentric phase previous to a CMJ or ABK jump.") +
+			"\n" +   Catalog.GetString("This jumper should change his technique, but if it's difficult, a solution is to increase threshold.");
+		textview_jumps.Buffer = tb_jumps;
+
+		TextBuffer tb_races = new TextBuffer (new TextTagTable());
+		tb_races.Text =  Catalog.GetString("Default value: 10 ms.") +
+			"\n\n" + Catalog.GetString("On races with photocells a value of 10 ms is the default value.") +
+			"\n\n" + Catalog.GetString("As Chronojump manages double contacts on photocells, changing threshold value is not very common.");
+		textview_races.Buffer = tb_races;
+
+		TextBuffer tb_other = new TextBuffer (new TextTagTable());
+		tb_other.Text =  Catalog.GetString("Default value: 50 ms.") +
+			"\n\n" + Catalog.GetString("Depending on the test, user could change values.");
+		textview_other.Buffer = tb_other;
 	}
 
 	public void on_button_close_clicked (object obj, EventArgs args) {

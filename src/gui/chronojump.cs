26a3f3254f6a7c6b03cfc81ce9a3b5ed7547c349
Xavier de Blas <xaviblas@gmail.com>
2017-01-05 20:34:36 +0100

    guiTests continues after a contacts test

--- src/gui/chronojump.cs
+++ src/gui/chronojump.cs
@@ -3998,7 +3998,7 @@ public partial class ChronoJumpWindow
 			updateRestTimes();
 		}
 
-		//chronojumpWindowTestsNext();
+		chronojumpWindowTestsNext();
 	}
 
 	//called each second and after a test

a65c75e2789108e29a9739b4f4ce7b8daeb1e26c
Xavier de Blas <xaviblas@gmail.com>
2017-03-22 23:55:02 +0100

    sprint communication 75% done

--- src/gui/sprint.cs
+++ src/gui/sprint.cs
@@ -23,11 +23,13 @@ using Gtk;
 using Glade;
 using System.Text; //StringBuilder
 using System.Collections; //ArrayList
+using System.Collections.Generic; //List<T>
 
 
 public partial class ChronoJumpWindow
 {
 	[Widget] Gtk.Button button_sprint;
+	static Sprint sprint;
 
 	private void createTreeView_runs_interval_sprint (Gtk.TreeView tv)
 	{
@@ -37,14 +39,15 @@ public partial class ChronoJumpWindow
 		tv.HeadersVisible=true;
 
 		int count = 0;
-		//tv.AppendColumn (Catalog.GetString ("Type"), new CellRendererText(), "text", count++);
-		//tv.AppendColumn (Catalog.GetString ("ID"), new CellRendererText(), "text", count++);
-		//tv.AppendColumn (Catalog.GetString ("Total time"), new CellRendererText(), "text", count++);
 		tv.AppendColumn ("Type", new CellRendererText(), "text", count++);
 		tv.AppendColumn ("ID", new CellRendererText(), "text", count++);
+		tv.AppendColumn ("Distances", new CellRendererText(), "text", count++);
+		tv.AppendColumn ("Split times", new CellRendererText(), "text", count++);
 		tv.AppendColumn ("Total time", new CellRendererText(), "text", count++);
 
-		TreeStore store = new TreeStore(typeof (string), typeof (string), typeof (string));
+		TreeStore store = new TreeStore(
+				typeof (string), typeof (string), typeof (string),
+				typeof (string), typeof (string));
 		tv.Model = store;
 
 		if (currentSession == null || currentPerson == null)
@@ -53,17 +56,52 @@ public partial class ChronoJumpWindow
 		tv.Selection.Changed -= onTreeviewSprintSelectionEntry;
 		tv.Selection.Changed += onTreeviewSprintSelectionEntry;
 
-		string [] array = SqliteRunInterval.SelectRuns(false, currentSession.UniqueID, currentPerson.UniqueID, "");
-		foreach (string line in array)
+		List<object> runITypes = SqliteRunIntervalType.SelectRunIntervalTypesNew("", false);
+		string [] runsArray = SqliteRunInterval.SelectRuns(
+				false, currentSession.UniqueID, currentPerson.UniqueID, "");
+
+		foreach (string line in runsArray)
 		{
 			string [] lineSplit = line.Split(new char[] {':'});
+			string positions = runIntervalDistances(lineSplit[4], runITypes);
+			if(positions == "")
+				continue;
+
+			positions = Util.ChangeChars(positions, "-", ";");
+
+			string intervalTimes = lineSplit[8];
+			string [] intervalTimesSplit = intervalTimes.Split(new char[] {'='});
+
+			//accumulated
+			double timeAccumulated = 0;
+			string splitTimes = "";
+			string sep = "";
+			foreach(string time in intervalTimesSplit)
+			{
+				double timeD = Convert.ToDouble(time);
+				timeAccumulated += timeD;
+				splitTimes += sep + Util.TrimDecimals(timeAccumulated, preferences.digitsNumber);
+				sep = ";";
+			}
+
 			string [] lineParams = { 
 				lineSplit[4],
-				lineSplit[1], 
+				lineSplit[1],
+				positions,
+				splitTimes,
 				Util.TrimDecimals(lineSplit[6], preferences.digitsNumber)
 			};
 			store.AppendValues (lineParams);
 		}
+	}
+
+	private string runIntervalDistances(string runTypeEnglishName, List<object> runITypes)
+	{
+		foreach(SelectRunITypes type in runITypes)
+			if(type.NameEnglish == runTypeEnglishName)
+				return(type.DistancesString);
+
+		return "";
 	}
 
 	private void onTreeviewSprintSelectionEntry (object o, EventArgs args)
@@ -77,9 +115,44 @@ public partial class ChronoJumpWindow
 		}
 	}
 
+	public static bool GetSelectedSprint (Gtk.TreeView tv)
+	{
+		TreeModel model;
+		TreeIter iter;
+
+		if (tv.Selection.GetSelected (out model, out iter))
+		{
+			 string positions = (string) model.GetValue(iter, 2);
+			 positions = Util.ChangeChars(positions, ",", ".");
+			 //add start TODO: check if needed
+			 positions = "0;" + positions;
+
+			 string splitTimes = (string) model.GetValue(iter, 3);
+			 splitTimes = Util.ChangeChars(splitTimes, ",", ".");
+			 //add start TODO: check if needed
+			 splitTimes = "0;" + splitTimes;
+
+			 sprint = new Sprint(
+					 positions,
+					 splitTimes,
+					 currentPersonSession.Weight, //TODO: can be more if extra weight
+					 currentPersonSession.Height,
+					 25);
+			return true;
+		}
+		return false;
+	}
+
+
 	private void on_button_sprint_clicked (object o, EventArgs args)
 	{
-		//test calling sprint.R file
-		new Sprint();
+		if(! GetSelectedSprint(treeview_runs_interval_sprint))
+		{
+			new DialogMessage(Constants.MessageTypes.WARNING, "Error");
+			return;
+		}
+
+		sprint.CallR();
+		new DialogMessage(Constants.MessageTypes.WARNING, "Ok");
 	}
 }

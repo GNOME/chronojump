999729e82b63aa5abcbb3f1bd45223a660db9ca0
Xavier de Blas <xaviblas@gmail.com>
2017-04-27 22:03:30 +0200

    slideNext/Prior now don't crash if Impress is not running

--- testing-stuff/slideNext.sh
+++ testing-stuff/slideNext.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+#works when Impress is not fullscreen
 export DISPLAY=:0.0
 WINDOW=`xdotool search --name "Impress"`
 #WINDOW=52428837
-xdotool windowactivate $WINDOW
-xdotool windowfocus $WINDOW
-xdotool key Next
+if ! test -z $WINDOW
+then
+	#echo $WINDOW
+	xdotool windowactivate $WINDOW
+	xdotool windowfocus $WINDOW
+	xdotool key Next
+fi

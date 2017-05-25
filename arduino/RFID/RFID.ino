e3c0fd6c5a26819be5a3b5ffad2629c413cf8343
Xavier de Blas <xaviblas@gmail.com>
2017-05-25 11:05:10 +0200

    Updated RFID.ino

--- arduino/RFID/RFID.ino
+++ arduino/RFID/RFID.ino
@@ -1,26 +1,47 @@
-#include <SPI.h>    //Cargamos la librería SPI incluida en el IDE de Arduino
-#include <RFID.h>   //Cargamos la librería RC522_RFID de Paul Kourany
+#include <SPI.h>
+
+#include <SPI.h>    //Load SPI on Arduino IDE
+#include <RFID.h>   //Load RC522_RFID library (Paul Kourany)
  
-#define SS_PIN 10   // Declaramos el pin SDA del Arduino
-#define RST_PIN 9   // Declaramos el pin RST del Arduino
-RFID rfid(SS_PIN, RST_PIN);   //Iniciamos el objeto RFID
+#define SS_PIN 10   // Declare SDA pin on Arduino
+#define RST_PIN 9   // Declare RST pin on Arduino
+RFID rfid(SS_PIN, RST_PIN);   //Start RFID object
  
-String cardID;      //Declaramos una variable de tipo string 
-                    //para almacenar el valor de los datos obtenidos de
-                    //la etiqueta RFID
+String cardID;      //Declare string variable to print RFID value
+
+String inputString = "";
  
 void setup() { 
-Serial.begin(9600); //Iniciamos la comunicación serie para leer las respuestas del módulo
-SPI.begin();        //Iniciamos la comunicación SPI
-rfid.init();        //Iniciamos el objeto RFID
+	Serial.begin(9600); //Start serial
+	SPI.begin();        //Start SPI
+	rfid.init();        //Start RFID object
 }
  
 void loop() {
-  if (rfid.isCard()) {   //Si hay una tarjeta cerca del lector...
-    if (rfid.readCardSerial()) {     //Leemos la ID de la tarjeta
-      cardID = String(rfid.serNum[0]) + "," + String(rfid.serNum[1]) + "," + String(rfid.serNum[2]) + 
-      "," + String(rfid.serNum[3]) + "," + String(rfid.serNum[4]);  //Le damos una formato de cadena de carácteres
-      Serial.println(cardID);  //La mostramos en el monitor serie
-      }
-    }
+	if (rfid.isCard()) {   //If there is a card close to the reader ...
+		if (rfid.readCardSerial()) {     //Read the card ID
+			cardID = String(rfid.serNum[0]) + "," + String(rfid.serNum[1]) + "," + String(rfid.serNum[2]) +
+				"," + String(rfid.serNum[3]) + "," + String(rfid.serNum[4]);  //Convert to str
+			cardID += ";"; 		//final mark
+			Serial.println(cardID); 	//show on serial monitor
+		}
+	}
+	delay(50);
+}
+
+void serialEvent()
+{
+	while (Serial.available())
+	{
+		char inChar = (char)Serial.read();
+		inputString += inChar;
+		if (inChar == '\n')
+		{
+			if (inputString.startsWith("Chronojump RFID"))
+			{
+				Serial.println("YES Chronojump RFID");
+			}
+			inputString = "";
+		}
+	}
 }

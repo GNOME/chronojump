9e136927129fd6c7d46989be57f9091183871c34
Xavier de Blas <xaviblas@gmail.com>
2017-07-04 11:39:47 +0200

    Fixed sensitivity bug on last commit

--- src/gui/encoder.cs
+++ src/gui/encoder.cs
@@ -3897,6 +3897,7 @@ public partial class ChronoJumpWindow
 		hbox_encoder_configuration.Sensitive = ! show;
 		hbox_encoder_capture_top.Sensitive = ! show;
 		vpaned_encoder_capture_video_and_set_graph.Sensitive = ! show;
+		button_encoder_inertial_recalibrate.Sensitive = ! show;
 	}
 
 	//info is now info and edit (all values can be changed), and detete (there's delete button)

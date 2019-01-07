d571fae0f39080aa80c770f1759449fa0f784fe6
Xavier de Blas <xaviblas@gmail.com>
2019-01-07 14:23:30 +0100

    Added license to ForceSensor4Binary.ino

--- arduino/ForceSensor4Binary/ForceSensor4Binary.ino
+++ arduino/ForceSensor4Binary/ForceSensor4Binary.ino
@@ -1,3 +1,20 @@
+/*
+  Copyright (C) 2019 Xavier Padullés support@chronojump.org
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
 #include <EEPROM.h>
@@ -6,8 +23,8 @@ Adafruit_ADS1015 loadCell;
 
 
 //Version number
-//it always need to start with: "Force_Platform-"
-String version = "Force_Platform-0.1";
+//it always need to start with: "Force_Sensor-" Since 0.3 data is sent in binary format
+String version = "Force_Sensor-0.3";
 
 int offset[] = {0, 0, 0, 0};
 float calibrationFactor[] = {0.0, 0.0, 0.0, 0.0};

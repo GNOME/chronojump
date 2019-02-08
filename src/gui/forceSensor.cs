c29efa07f8a48466758fb521c0abaf4bdb90db98
Xavier Padullés <x.padulles@gmail.com>
2019-02-08 17:45:27 +0100

    Testing arduino communicationa at 1Mbit/s

--- src/gui/forceSensor.cs
+++ src/gui/forceSensor.cs
@@ -190,7 +190,7 @@ public partial class ChronoJumpWindow
 		forceSensorOtherMessage = "Connecting ...";
 
 		portFS = new SerialPort(forceSensorPortName, 115200); //forceSensor
-		//portFS = new SerialPort(forceSensorPortName, 250000); //forceSensor
+		//portFS = new SerialPort(forceSensorPortName, 1000000); //forceSensor
 		LogB.Information(" FS connect 4: opening port...");
 
 		try {

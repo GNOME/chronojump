4462ccec29ac770e0b90c00a00927d63edeeb96c
Xavier Padullés <x.padulles@gmail.com>
2019-03-12 14:17:30 +0100

    RaceAnalyzer. Introduced force sensor calculations

--- r-scripts/scripts-util.R
+++ r-scripts/scripts-util.R
@@ -7,12 +7,8 @@ interpolateXAtY <- function(X, Y, desiredY){
                 
         #find the closest sample
         nextSample = 1
-        print("X:")
-        print(X)
-        print("Y:")
-        print(Y)
-        print("desiredY:")
-        print(desiredY)
+        print("Calculating the interpolation")
+        print(paste("desiredY:", desiredY))
         while (Y[nextSample] < desiredY){
                 nextSample = nextSample +1
         }
@@ -20,11 +16,63 @@ interpolateXAtY <- function(X, Y, desiredY){
         previousSample = nextSample - 1
         
         if(Y[nextSample] == desiredY){
-                desiredX = X[nextSample]
+                correspondingX = X[nextSample]
         } else {
-                desiredX = X[previousSample] + (desiredY  - Y[previousSample]) * (X[nextSample] - X[previousSample]) / (Y[nextSample] - Y[previousSample])
+                correspondingX = X[previousSample] + (desiredY  - Y[previousSample]) * (X[nextSample] - X[previousSample]) / (Y[nextSample] - Y[previousSample])
         }
-        return(desiredX)
+        print(paste("CorrespondingX:", correspondingX))
+        return(correspondingX)
+}
+
+
+#Calculate the area under a curve using the cross product of consecutive vectors with the origin in the first point
+getAreaUnderCurve <- function(x, y)
+{
+        print("Calculating Area")
+        print("X:")
+        print(x)
+        print("Y:")
+        print(y)
+        x = c(x, x[length(x)], x[1])
+        y = c(y, 0, 0)
+        totalArea = 0
+        print(paste("V",1," = ", "(",x[1 + 1] - x[1],",", y[1 + 1] - y[1], ")", sep = ""))
+        for(i in 2:(length(x) -1))
+        {
+                parallelogramArea = ((x[i + 1] - x[1])* (y[i] - y[1]) - (x[i] - x[1]) * (y[i+1] - y[1]))
+                
+                # print(paste("V",i," = ", "(",x[i + 1] - x[1],",", y[i + 1] - y[1], ")", sep = ""))
+                # print(parallelogramArea/2)
+                
+                totalArea = totalArea + parallelogramArea
+        }
+        print(paste("toalArea:", totalArea/2))
+        return(totalArea/2) #The area of the parallelograms are twice the triangles areas
+}
+
+#Calculates the mean of a curve interval
+getMeanValue <- function(X, Y, startX, endX)
+{
+        print(paste("Calculating mean In the X range of [", startX, ",", endX, "]"))
+        
+        
+        #Calculating the value of Y corresponding at startX
+        print("Calculating the first Y value")
+        startY = interpolateXAtY(X = Y, Y = X, desiredY = startX) #The order are changed because we are looking for the Y value instead of X value
+        
+        #Calculating the last value using the interpolation
+        print("Calculating the last Y value")
+        endY = interpolateXAtY(X = Y, Y = X, desiredY = endX) #The order are changed because we are looking for the Y value instead of X value
+
+        X = X[which(X > startX & X < endX)]
+        X = c(startX, X, endX)
+        
+        Y = Y[which(X > startX & X < endX)]
+        Y = c(startY, Y, endY)
+        
+        #calculating the area under the curve (integral)
+        area = getAreaUnderCurve(X , Y)
+        return(area / (X[length(X)] - X[1]))
 }
 
 prepareGraph <- function(os, pngFile, width, height)

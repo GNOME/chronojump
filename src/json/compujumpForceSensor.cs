023d33c8cb309d9d216ae8480d1bd1787a550008
Xavier de Blas <xaviblas@gmail.com>
2020-01-30 12:05:36 +0100

    Organized networks compujump code

---
+++ src/json/compujumpForceSensor.cs
@@ -0,0 +1,114 @@
+/*
+ * This file is part of ChronoJump
+ *
+ * ChronoJump is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or   
+ *    (at your option) any later version.
+ *    
+ * ChronoJump is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ *    GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Copyright (C) 2016-2017 Carles Pina & Xavier de Blas
+ */
+
+using System;
+using System.Text;
+using System.Collections;
+
+
+public class UploadForceSensorDataFullObject
+{
+	public int uniqueId; //used for SQL load and delete
+	public int personId;
+	public int stationId;
+	public int exerciseId;
+	public string laterality;
+	public string resistance; //stiffness
+	public UploadForceSensorDataObject uo;
+
+	public UploadForceSensorDataFullObject(int uniqueId, int personId, int stationId, int exerciseId,
+			string laterality, string resistance, UploadForceSensorDataObject uo)
+	{
+		this.uniqueId = uniqueId;
+		this.personId = personId;
+		this.stationId = stationId;
+		this.exerciseId = exerciseId;
+		this.laterality = laterality;
+		this.resistance = resistance;
+		this.uo = uo;
+	}
+
+	public string ToSQLInsertString ()
+	{
+		return
+			"NULL, " +
+			personId.ToString() + ", " +
+			stationId.ToString() + ", " +
+			exerciseId.ToString() + ", " +
+			"\"" + laterality + "\", " +
+			"\"" + resistance + "\", " +
+			uo.ToString();
+	}
+}
+
+public class UploadForceSensorDataObject
+{
+	public string variability;
+	public string timeTotal;
+	public string impulse;
+	public string workJ;
+	public int repetitions;
+	public int numRep;
+	public string repCriteria;
+	public string time;  //duration?
+	public string range;
+	public string fmaxRaw;
+	public string rfdmeanRaw;
+	public string rfdmaxRaw;
+	public string fmaxModel;
+	public string rfdmaxModel;
+	//only on elastic
+	public string vmean;
+	public string vmax;
+	public string amean;
+	public string amax;
+	public string pmean;
+	public string pmax;
+
+	//constructor called after capture
+	public UploadForceSensorDataObject()
+	{
+	}
+
+	public override string ToString()
+	{
+		return
+			"\"" + variability.ToString() + "\", " +
+			"\"" + timeTotal.ToString() + "\"," +
+			"\"" + impulse.ToString() + "\"," +
+			"\"" + workJ.ToString() + "\"," +
+			repetitions.ToString() + "," +
+			numRep.ToString() + "," +
+			"\"" + repCriteria.ToString() + "\", " +
+			"\"" + time.ToString() + "\"," +
+			"\"" + range.ToString() + "\"," +
+			"\"" + fmaxRaw.ToString() + "\"," +
+			"\"" + rfdmeanRaw.ToString() + "\"," +
+			"\"" + rfdmaxRaw.ToString() + "\"," +
+			"\"" + fmaxModel.ToString() + "\"," +
+			"\"" + rfdmaxModel.ToString() + "\"," +
+			"\"" + vmean.ToString() + "\"," +
+			"\"" + vmax.ToString() + "\"," +
+			"\"" + amean.ToString() + "\"," +
+			"\"" + amax.ToString() + "\"," +
+			"\"" + pmean.ToString() + "\"," +
+			"\"" + pmax.ToString() + "\")";
+	}
+}
---
+++ src/json/compujumpOther.cs
@@ -0,0 +1,134 @@
+/*
+ * This file is part of ChronoJump
+ *
+ * ChronoJump is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or   
+ *    (at your option) any later version.
+ *    
+ * ChronoJump is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ *    GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Copyright (C) 2016-2017 Carles Pina & Xavier de Blas
+ */
+
+using System;
+using System.Text;
+using System.Collections;
+using Mono.Unix;
+
+
+public class Task
+{
+	public int Id;
+	public char Type; //initially 'P'arametrized or 'F'ree. Now all are 'P'
+	public int PersonId;
+	public int StationId;
+	public int ExerciseId;
+	public string ExerciseName;
+	public int Sets;
+	public int Nreps;
+	public float Load;
+	public float Speed;
+	public float PercentMaxSpeed;
+	public string Laterality;
+	public string Comment;
+
+	public Task()
+	{
+		Id = -1;
+		Comment = "";
+	}
+
+	public Task(int id, int personId, int stationId, int exerciseId, string exerciseName,
+			int sets, int nreps, float load, float speed, float percentMaxSpeed,
+			string laterality, string comment)
+	{
+		Type = 'P'; //parametrized
+
+		Id = id;
+		PersonId = personId;
+		StationId = stationId;
+		ExerciseId = exerciseId;
+		ExerciseName = exerciseName;
+		Sets = sets;
+		Nreps = nreps;
+		Load = load;
+		Speed = speed;
+		PercentMaxSpeed = percentMaxSpeed;
+		Laterality = laterality;
+		Comment = comment;
+	}
+
+	public override string ToString()
+	{
+		string sep = "";
+		string str = "";
+		if (Laterality == "R" || Laterality == "L")
+		{
+			string lateralityStr = Catalog.GetString("Right");
+			if (Laterality == "L")
+				lateralityStr = Catalog.GetString("Left");
+
+			str += sep + lateralityStr;
+			sep = "; ";
+		}
+		if (Load != -1)
+		{
+			str += sep + "Càrrega = " + Load.ToString() + " Kg";
+			sep = "; ";
+		}
+		if (Sets != -1)
+		{
+			str += sep + "Series = " + Sets.ToString();
+			sep = "; ";
+		}
+		if (Nreps != -1)
+		{
+			str += sep + "Repeticions = " + Nreps.ToString();
+			sep = "; ";
+		}
+		if (Speed != -1)
+		{
+			str += sep + "Velocitat = " + Speed.ToString() + " m/s";
+			sep = "; ";
+		}
+		if (PercentMaxSpeed != -1)
+		{
+			str += sep + "Velocitat = " + PercentMaxSpeed.ToString() + " %";
+			sep = "; ";
+		}
+		if (Comment != "")
+		{
+			str += "\n" + Comment;
+		}
+		return ExerciseName + ": " + str;
+	}
+}
+
+public class StationCount
+{
+	private string stationName;
+	private int tasksCount;
+
+	public StationCount()
+	{
+	}
+
+	public StationCount(string name, int count)
+	{
+		stationName = name;
+		tasksCount = count;
+	}
+
+	public override string ToString()
+	{
+		return stationName + " (" + tasksCount.ToString() + ")";
+	}
+}

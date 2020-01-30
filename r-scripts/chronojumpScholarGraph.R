15dad8cd5e58a6db3b87c09143fd160117090e0d
Xavier de Blas <xaviblas@gmail.com>
2020-01-30 18:17:08 +0100

    Added chronojumpScholarGraph.R

---
+++ r-scripts/chronojumpScholarGraph.R
@@ -0,0 +1,5 @@
+years = seq(from=2004, to=2019, by=1)
+results = c(1,5,2,1,0,1,1,5,10,10,22,20,44,51,70,81)
+png(filename="chronojumpScholarGraph.png", width=756, height=500, units="px")
+barplot(results, names.arg=years, las=2, main="Results of 'Chronojump' on Google Scholar by years", sub="Excluding cites and patents", col=rev(heat.colors(length(years))))
+dev.off()

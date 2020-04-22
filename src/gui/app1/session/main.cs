de2ba33907ec5c7f7e693213ab35d7c72945ba67
Xavier de Blas <xaviblas@gmail.com>
2020-04-22 09:32:52 +0200

    eventboxes for session buttons

--- src/gui/app1/session/main.cs
+++ src/gui/app1/session/main.cs
@@ -30,6 +30,8 @@ public partial class ChronoJumpWindow
 	[Widget] Gtk.Notebook app1s_notebook;
 
 	//notebook tab 0
+	[Widget] Gtk.EventBox app1s_eventbox_button_close0;
+
 	//notebook tab 1
 	[Widget] Gtk.HBox hbox_session_more;
 	[Widget] Gtk.VBox vbox_session_overview;
@@ -38,6 +40,7 @@ public partial class ChronoJumpWindow
 	[Widget] Gtk.Image app1s_image_open_database;
 	[Widget] Gtk.Label app1s_label_open_database_file;
 	[Widget] Gtk.Button app1s_button_select_file_import_same_database;
+	[Widget] Gtk.EventBox app1s_eventbox_button_cancel1;
 
 	//notebook tab 2
 	[Widget] Gtk.TreeView app1s_treeview_session_load;
@@ -51,11 +54,17 @@ public partial class ChronoJumpWindow
 	[Widget] Gtk.Label app1s_file_path_import;
 	[Widget] Gtk.Notebook app1s_notebook_load_button_animation;
 	[Widget] Gtk.HButtonBox app1s_hbuttonbox_page2_import;
+	[Widget] Gtk.EventBox app1s_eventbox_button_cancel;
+	[Widget] Gtk.EventBox app1s_eventbox_button_accept;
+	[Widget] Gtk.EventBox app1s_eventbox_button_back;
+	[Widget] Gtk.EventBox app1s_eventbox_button_import;
 
 	//notebook tab 3
 	[Widget] Gtk.Label app1s_label_import_session_name;
 	[Widget] Gtk.Label app1s_label_import_file;
 	[Widget] Gtk.Button app1s_button_import_confirm_accept;
+	[Widget] Gtk.EventBox app1s_eventbox_button_import_confirm_back;
+	[Widget] Gtk.EventBox app1s_eventbox_button_import_confirm_accept;
 
 	//notebook tab 4
 	[Widget] Gtk.ProgressBar app1s_progressbarImport;
@@ -65,6 +74,12 @@ public partial class ChronoJumpWindow
 	[Widget] Gtk.TextView app1s_textview_import_error;
 	[Widget] Gtk.Image app1s_image_import1;
 	[Widget] Gtk.HButtonBox app1s_hbuttonbox_page4;
+	[Widget] Gtk.EventBox app1s_eventbox_button_import_close;
+	[Widget] Gtk.EventBox app1s_eventbox_button_import_again;
+
+	//notebook tab 5
+	[Widget] Gtk.EventBox app1s_eventbox_button_delete_close;
+
 
 	const int app1s_PAGE_MODES = 0;
 	const int app1s_PAGE_IMPORT_START = 1;
@@ -90,6 +105,21 @@ public partial class ChronoJumpWindow
 		//but if it is start page, ensure notebook_mode_selector is 0
 		if(notebook_sup.CurrentPage == Convert.ToInt32(notebook_sup_pages.START))
 			notebook_mode_selector.CurrentPage = 0;
+	}
+
+	private void app1s_eventboxes_paint()
+	{
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_close0, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_cancel1, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_cancel, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_accept, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_back, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_import_confirm_back, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_import_confirm_accept, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_import, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_import_close, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_import_again, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
+		UtilGtk.EventBoxColorBackgroundActive (app1s_eventbox_button_delete_close, UtilGtk.YELLOW, UtilGtk.YELLOW_LIGHT);
 	}
 
 }

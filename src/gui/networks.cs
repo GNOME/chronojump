572a3887b9a0da701af896217fbc753f2d5d7412
Xavier de Blas <xaviblas@gmail.com>
2020-04-22 11:37:49 +0200

    session buttons not available on UNIQUE && MONTHLY

--- src/gui/networks.cs
+++ src/gui/networks.cs
@@ -423,6 +423,11 @@ public partial class ChronoJumpWindow
 			//hbox_menu_and_preferences_outside_menu_contacts.Visible = true;
 			//hbox_menu_and_preferences_outside_menu_encoder.Visible = true;
 
+			//hide session controls on UNIQUE and MONTHLY
+			vbox_menu_session.Visible = false;
+			vbox_menu_session1.Visible = false;
+
+
 			if(configChronojump.SessionMode == Config.SessionModeEnum.UNIQUE)
 			{
 				if(! Sqlite.Exists(false, Constants.SessionTable, "session")) {

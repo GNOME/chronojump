b92cb3993b459508111669b6ed61ee760d9c0c1d
Xavier de Blas <xaviblas@gmail.com>
2020-04-22 12:39:07 +0200

    Updated copyright notices

--- src/chronojumpImporter.cs
+++ src/chronojumpImporter.cs
@@ -24,7 +24,7 @@ using Mono.Unix;
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Copyright (C) 2016-2017   Carles Pina i Estany <carles@pina.cat>
- * Copyright (C) 2019   Xavier de Blas <xaviblas@gmail.com>
+ * Copyright (C) 2019-2020   Xavier de Blas <xaviblas@gmail.com>
  */
 
 /*

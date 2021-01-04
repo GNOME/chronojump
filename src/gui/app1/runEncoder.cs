fcbd397810d36dfc53101ba939e794d623fc2a22
Xavier Padullés <x.padulles@gmail.com>
2021-01-04 17:44:21 +0100

    RaceAnalyzer. Fixed speed shown during capture

--- src/gui/app1/runEncoder.cs
+++ src/gui/app1/runEncoder.cs
@@ -545,7 +545,7 @@ public partial class ChronoJumpWindow
 				if(timePre > 0)
 				{
 					double runEncoderCaptureDistanceAtThisSample = Math.Abs(encoderDisplacement) * 0.003003; //hardcoded: same as sprintEncoder.R
-					runEncoderCaptureSpeed = UtilAll.DivideSafe(runEncoderCaptureDistanceAtThisSample, (time - timePre));
+					runEncoderCaptureSpeed = UtilAll.DivideSafe(runEncoderCaptureDistanceAtThisSample, (time - timePre)) * 1000000;
 
 					runEncoderCaptureDistance += runEncoderCaptureDistanceAtThisSample;
 

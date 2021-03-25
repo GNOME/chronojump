7d7921b8382301f5353f6078f172b6f42680f5fc
Xavier de Blas <xaviblas@gmail.com>
2021-03-25 16:28:12 +0100

    Updated chronojumpScholarGraph.R

--- r-scripts/chronojumpScholarGraph.R
+++ r-scripts/chronojumpScholarGraph.R
@@ -1,5 +1,5 @@
 years = seq(from=2004, to=2020, by=1)
-results = c(1,5,2,1,0,3,2,6,14,12,26,24,45,50,79,98,140)
+results = c(1,4,2,1,0,1,2,4,10,10,24,21,44,51,69,100,153) #2021: 31 at March 25
 
 png(filename="chronojumpScholarGraph.png", width=756, height=500, units="px")
 bp = barplot(results, names.arg=years, las=2, main="New results of 'Chronojump' on Google Scholar by years", sub="Excluding cites and patents", col=rev(heat.colors(length(years))))
@@ -7,4 +7,5 @@ fit <- lm(results ~ poly(bp[,1], 3))
 x0 <- seq(min(bp[,1]), max(bp[,1]), length = length(results))
 y0 <- predict.lm(fit, newdata = list(x = x0))
 lines(x0,y0, lwd=2)
+mtext(side=3, at=x0[length(x0)], results[length(x0)])
 dev.off()

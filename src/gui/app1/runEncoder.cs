be0c1d027b9f62a7c4624ac7167252b04530c879
Xavier de Blas <xaviblas@gmail.com>
2021-04-07 12:42:30 +0200

    After race analyzer capture, now analyze button is sensitive

--- src/gui/app1/runEncoder.cs
+++ src/gui/app1/runEncoder.cs
@@ -920,6 +920,7 @@ public partial class ChronoJumpWindow
 		sensitiveLastTestButtons(true);
 
 		event_execute_label_message.Text = "Loaded: " + Util.GetLastPartOfPath(re.Filename);
+		button_run_encoder_analyze_analyze.Sensitive = true;
 		button_run_encoder_analyze_options_close_and_analyze.Sensitive = true;
 		button_run_encoder_image_save.Sensitive = true;
 	}

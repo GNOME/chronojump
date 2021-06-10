de16ac293ca946009dd8b6a058484939ed211281
Xavier de Blas <xaviblas@gmail.com>
2021-06-10 17:47:04 +0200

    Fixed label_sprint_export_data was not showing person or session (was not showing anything)

--- src/gui/app1/chronojump.cs
+++ src/gui/app1/chronojump.cs
@@ -1336,6 +1336,11 @@ public partial class ChronoJumpWindow
 				label_sprint_person_name.Text = string.Format(Catalog.GetString("Sprints of {0}"), currentPerson.Name);
 			createTreeView_runs_interval_sprint (treeview_runs_interval_sprint);
 
+			if(currentPerson != null)
+				label_sprint_export_data.Text = currentPerson.Name;
+			else
+				label_sprint_export_data.Text = "";
+
 			label_sprint_export_result.Text = "";
 			button_sprint_export_result_open.Visible = false;
 		}

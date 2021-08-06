9db3dffe7a493b7225720d96b4b737d841e95879
Xavier de Blas <xaviblas@gmail.com>
2021-08-06 07:31:00 +0200

    treeview events repair buttons with bigger image

--- src/gui/app1/icons.cs
+++ src/gui/app1/icons.cs
@@ -469,15 +469,15 @@ public partial class ChronoJumpWindow
 		image_contacts_recalculate.Pixbuf = pixbuf;
 
 		pixbuf = new Pixbuf (null, Util.GetImagePath(false) + "image_build_24.png");
+		image_jump_reactive_repair.Pixbuf = pixbuf;
+		image_run_interval_repair.Pixbuf = pixbuf;
+		image_pulse_repair.Pixbuf = pixbuf;
+		image_multi_chronopic_repair.Pixbuf = pixbuf;
 		image_encoder_configuration.Pixbuf = pixbuf;
 		image_force_sensor_capture_adjust.Pixbuf = pixbuf;
 		image_force_sensor_analyze_options.Pixbuf = pixbuf;
 		image_force_sensor_analyze_options1.Pixbuf = pixbuf;
 		pixbuf = new Pixbuf (null, Util.GetImagePath(false) + "image_build_16.png");
-		image_jump_reactive_repair.Pixbuf = pixbuf;
-		image_run_interval_repair.Pixbuf = pixbuf;
-		image_pulse_repair.Pixbuf = pixbuf;
-		image_multi_chronopic_repair.Pixbuf = pixbuf;
 		image_encoder_analyze_mode_options.Pixbuf = pixbuf;
 
 		//assign here to have gui ok and not having chronojump logo at top right outside the screen

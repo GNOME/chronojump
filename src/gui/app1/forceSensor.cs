ac824c3097d68d4ca5cc4a7aaff80db8332321d3
Xavier de Blas <xaviblas@gmail.com>
2022-01-04 13:26:15 +0100

    cont of last commit but adding (N)

--- src/gui/app1/forceSensor.cs
+++ src/gui/app1/forceSensor.cs
@@ -2170,8 +2170,8 @@ LogB.Information(" fs R ");
 			Catalog.GetString("Elastic") + " (N/m)",
 			Catalog.GetString("Capture option"),
 			Catalog.GetString("Laterality"),
-			"Max force" + " (" + Catalog.GetString("Raw Data") + ")", //TODO: translate
-			string.Format("Max AVG Force in {0} s", 1), //TODO: translate
+			"Max force" + " (" + Catalog.GetString("Raw Data") + ") (N)", //TODO: translate
+			string.Format("Max AVG Force in {0} s", 1) + " (N)", //TODO: translate
 			Catalog.GetString("Date"),
 			Catalog.GetString("Video"),
 			Catalog.GetString("Comment")

612e9a9f1542e4e6b443445ffcc162bc6440ae6c
Xavier de Blas <xaviblas@gmail.com>
2022-05-09 12:46:32 +0200

    CairoGraphRaceAnalyzer prints RunEncoderSegmentCalcs accel, force

--- src/gui/cairo/raceAnalyzer.cs
+++ src/gui/cairo/raceAnalyzer.cs
@@ -136,7 +136,7 @@ public class CairoGraphRaceAnalyzer : CairoXY
 				//vertical
 				if(segmentCalcs.Count > 0)
 				{
-					LogB.Information("accels: ");
+					LogB.Information("accels ; forces");
 					g.Save();
 					g.SetDash(new double[]{1, 2}, 0);
 					for(int i = 0 ; i < segmentCalcs.Count ; i ++)
@@ -146,7 +146,9 @@ public class CairoGraphRaceAnalyzer : CairoXY
 						//seconds
 						string xTextBottom = Util.TrimDecimals(segmentCalcs.Time_l[i]/1000000.0, 1).ToString();
 						double xGraph = calculatePaintX(segmentCalcs.Time_l[i]/1000000.0);
-						LogB.Information(segmentCalcs.Accel_l[i].ToString());
+						LogB.Information(string.Format("{0} ; {1}",
+									segmentCalcs.Accel_l[i].ToString(),
+									segmentCalcs.Force_l[i].ToString() ));
 
 						if(useListOfDoublesOnY)
 							paintVerticalGridLine(g, Convert.ToInt32(xGraph), xTextBottom, textHeight-3);

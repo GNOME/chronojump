5d6c3f67a28749f1cb4096e9b336e33e3c5846f0
Xavier Padullés <testing@chronojump.org>
2022-06-08 21:18:04 +0200

    Separating and commenting functions in smaller files

--- arduino/michrolab/michrolab.h
+++ arduino/michrolab/michrolab.h
@@ -5,3 +5,114 @@ struct menuEntry {
     String description;
     functionPointer function;
 };
+
+//Starts reading from the load cell alone
+void startLoadCellCapture(void);
+
+//Calculations of the load cell data: Max, Mean, Impulse, RFD...
+void getLoadCellDynamics(void);
+
+void endLoadCellCapture();
+
+void showLoadCellResults();
+
+//Starts reading from the encoder alone
+void startEncoderCapture(void);
+
+void getEncoderDynamics();
+
+void showEncoderResults();
+
+void endEncoderCapture();
+
+//Sets the inertial mode on and sets the position of Con/Ecc change
+void startInertialEncoderCapture();
+
+//sets the position of Con/Ecc change
+void calibrateInertial();
+
+//Reads from the load cell and encoder simultaneously
+void startPowerCapture(void);
+
+void getPowerDynamics();
+
+void readEncoder();
+
+void endPowerCapture();
+
+void showPowerResults();
+
+//Starts to read from load cell and waits to press the red button to start the actual steadiness calculations
+void startSteadiness(void);
+
+void end_steadiness();
+
+//Reads from the RCA to calculate jump heights
+void startJumpsCapture();
+
+//Measuring the selected sensors as well as plotting and saving to SD
+//It also manage the buttons pressed
+void capture();
+
+//Prints a float number with the units number at the selected positoin and precission
+void printTftFormat (float val, int x, int y, int fontSize, int decimal);
+
+//Reads whatever it is in the serial buffer
+void serialEvent();
+
+//return the version of the firmware as well as the hardware type
+void get_version();
+
+//returns the load cell calibration factor
+void get_calibration_factor();
+
+//Sets the calibration factor of the load cell
+void set_calibration_factor(String inputString);
+
+//Process of the calibration. The inputString includes the load used to calibrate
+void calibrate(String inputString);
+
+//Process of offsetting the load cell
+void tare();
+
+//Process of tare and capture. Usefull when the body weight must be substracted from the load cell readings
+void startTareCapture(void);
+
+//Returns the offset of the load cell
+void get_tare();
+
+//Set the offset of the load cell
+void set_tare(String inputString);
+
+//Returns the argument of the command with the form [command]:[argument];
+String get_command_argument(String inputString);
+
+//Not used. Tells if the info sent to the Serial is in text or binary format
+void get_transmission_format();
+
+//Funcion called when the RCA state has changed
+void changingRCA();
+
+//Process of calibration controled from the tft
+void calibrateTFT(void);
+
+//function to read battery level. Not implemented in SportAnalyzer hardware version 1.0
+void showBatteryLevel();
+
+//Update the time in the graph every second
+void updateTime();
+
+//TODO: Add more information or eliminate
+void showSystemInfo(void);
+
+//Sets the horizontal line shown in steadiness measure
+void setForceGoal();
+
+//Saves the meadured data in the SD
+void saveSD(String fileName);
+
+//Count how many dirs exists. Used to create new dirs with the correct numeration
+int countDirs();
+
+//Process of changing the person with the blue button
+void selectPerson();
---
+++ arduino/michrolab/personsManage.ino
@@ -0,0 +1,141 @@
+void savePersonsList()
+{
+  SD.remove("persons.txt");
+  File personsFile = SD.open("persons.txt");
+  for (unsigned int i = 0; i < totalPersons; i++)
+  {
+    personsFile.print(persons[i].index);
+    personsFile.print("," + persons[i].name + "," + persons[i].surname + ",");
+    personsFile.print(persons[i].weight);
+    personsFile.print(",");
+    personsFile.println(persons[i].heigh);
+  }
+  personsFile.close();
+}
+
+void printPersonsList()
+{
+  for (unsigned int i = 0; i < totalPersons; i++)
+  {
+    Serial.print(persons[i].index);
+    Serial.print("," + persons[i].name + "," + persons[i].surname + ",");
+    Serial.print(persons[i].weight);
+    Serial.print(",");
+    Serial.println(persons[i].heigh);
+  }
+}
+
+void addPerson(String row)
+{
+  int prevComaIndex = row.indexOf(":");
+  int nextComaIndex = row.indexOf(",");
+  currentPerson = row.substring(prevComaIndex + 1, nextComaIndex).toInt();
+  persons[currentPerson].index = currentPerson;
+
+  if (currentPerson >= totalPersons) totalPersons = currentPerson + 1;
+  prevComaIndex = nextComaIndex;
+  nextComaIndex = row.indexOf(",", prevComaIndex + 1 );
+  persons[currentPerson].name = row.substring(prevComaIndex + 1 , nextComaIndex);
+  prevComaIndex = nextComaIndex;
+  nextComaIndex = row.indexOf(",", prevComaIndex + 1 );
+  persons[currentPerson].surname = row.substring(prevComaIndex + 1 , nextComaIndex);
+
+  prevComaIndex = nextComaIndex;
+  nextComaIndex = row.indexOf(",", prevComaIndex + 1 );
+  persons[currentPerson].weight = row.substring(prevComaIndex + 1 , nextComaIndex).toFloat();
+
+  prevComaIndex = nextComaIndex;
+  nextComaIndex = row.indexOf(",", prevComaIndex + 1 );
+  persons[currentPerson].heigh = row.substring(prevComaIndex + 1 , nextComaIndex).toFloat();
+}
+
+//void readPersonsFile(struct personType * persons)
+void readPersonsFile()
+{
+  /*
+     Ecample of persons.txt format
+    0,Blancaneus,160, 65,
+    1,Pulgarcito,16, 6,
+    3,Tres porquets,50, 20,
+  */
+  String row = "";
+  char readChar;
+  File  personsFile = SD.open("persons.txt");
+  if (personsFile)
+  {
+
+    currentPerson = 0;
+    personsFile.seek(0);
+
+    // read from the file until there's nothing else in it:
+    while (currentPerson < totalPersons)
+    {
+      readChar = personsFile.read();
+      if (readChar != '\n' && readChar != '\r')
+      {
+        row = row + readChar;
+      } else if (readChar == '\n' || readChar == '\r')
+      {
+        addPerson(row);
+        row = "";
+        currentPerson++;
+      }
+    }
+    // close the file:
+    personsFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening persons.txt");
+  }
+}
+
+unsigned int getTotalPerson()
+{
+  char readChar;
+  String readString = "";
+  File  personsFile = SD.open("persons.txt");
+  if (personsFile)
+  {
+    //Start reading from the last byte
+    unsigned long pos = personsFile.size() - 4;
+
+    //Reading the person number of the last row
+    while (readChar != '\n' && readChar != '\r')
+    {
+      personsFile.seek(pos);
+      readChar = personsFile.peek();
+      pos--;
+    }
+    pos++;
+    personsFile.seek(pos);
+    readChar = personsFile.read();
+    while (readChar != ',')
+    {
+      readChar = personsFile.read();
+      readString = readString + readChar;
+    }
+  }
+
+  totalPersons = readString.toInt() + 1;
+  return (totalPersons);
+}
+
+void updatePersonSet()
+{
+  String personSet = "Set: " + addLeadingZeros(setNumber, 2) + "   Person: " + addLeadingZeros(currentPerson, 2);
+  tft.setTextSize(1);
+  tft.fillRect(148, 207, 120, 8, BLACK);
+  tft.setTextColor(BLACK);
+  tft.setCursor(148, 223);
+  tft.print(persons[currentPerson].name + " " + persons[currentPerson].surname);
+  currentPerson = (currentPerson + 1) % totalPersons;
+
+  personSet = "Set: " + addLeadingZeros(setNumber, 2) + "   Person: " + addLeadingZeros(currentPerson, 2);
+  fileName = "S" + addLeadingZeros(setNumber, 2) + "P" + addLeadingZeros(currentPerson, 2);
+  tft.setTextColor(WHITE);
+  tft.setCursor(148, 207);
+  tft.print(personSet);
+  tft.setCursor(148, 223);
+  tft.print(persons[currentPerson].name + " " + persons[currentPerson].surname);
+  tft.setTextSize(2);
+}

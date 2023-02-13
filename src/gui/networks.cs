a7c3ab8c90cab5746eb0cb2e48e92f5a1a2bcd09
Xavier de Blas <xaviblas@gmail.com>
2023-02-13 16:10:38 +0100

    Fixed combos not updated after databaseChange. TODO: Need to test.

--- src/gui/networks.cs
+++ src/gui/networks.cs
@@ -15,7 +15,7 @@
  *  along with this program; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Copyright (C) 2004-2022   Xavier de Blas <xaviblas@gmail.com>
+ * Copyright (C) 2004-2023   Xavier de Blas <xaviblas@gmail.com>
  */
 
 
@@ -404,6 +404,8 @@ public partial class ChronoJumpWindow
 
 		label_current_database.UseMarkup = true;
 		label_current_database.TooltipText = configChronojump.LastDBFullPath;
+
+		fillAllCombos ();
 	}
 
 	Gtk.FileChooserDialog database_fc;

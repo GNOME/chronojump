450a03700ee2531549ba89dcd0aa62406ea3d64d
Xavier de Blas <xaviblas@gmail.com>
2023-03-09 16:01:21 +0100

    Compilation files for gtk3 (and removed duplicated Cairo linking)

--- build/build.environment.mk
+++ build/build.environment.mk
@@ -16,12 +16,11 @@ LINK_SYSTEM_XML = -r:System.Xml
 LINK_SYSTEM_XML_LINQ = -r:System.Xml.Linq
 LINK_SYSTEM_WEB_SERVICES = -r:System.Web.Services
 LINK_MICROSOFT_CSHARP = -r:Microsoft.CSharp
-LINK_CAIRO = -r:Mono.Cairo
+#LINK_CAIRO = -r:Mono.Cairo #Commented because cairo linking is duplicated "error CS0433: The imported type `Cairo.Context' is defined multiple times"
 LINK_MONO_POSIX = -r:Mono.Posix
 LINK_MONO_DATA_SQLITE = -r:Mono.Data.Sqlite
-LINK_GLIB = $(GLIB_SHARP_20_LIBS)
-LINK_GTK = $(GTK_SHARP_20_LIBS)
-LINK_GLADE = $(GLADE_SHARP_20_LIBS)
+LINK_GLIB = $(GLIB_SHARP_30_LIBS)
+LINK_GTK = $(GTK_SHARP_30_LIBS)
 LINK_JSON = -r:System.Json.dll
 
 REF_DEP_CHRONOJUMP = \
@@ -35,7 +34,7 @@ REF_DEP_CHRONOJUMP = \
 	$(LINK_GLIB) \
 	$(LINK_GLADE) \
 	$(LINK_GTK) \
-	$(LINK_JSON) \
+	$(LINK_JSON) #\
 	$(LINK_CAIRO)
 
 REF_DEP_CHRONOJUMP_SERVER = \

4349d688c1ccef97ecffbe6c1c6c21dba7ddc299
Xavier de Blas <xaviblas@gmail.com>
2023-05-02 19:45:38 +0200

    Updated encoder/tests/fixEccConCutOnNotSingleFile/compareSetSession.R

--- encoder/tests/fixEccConCutOnNotSingleFile/compareSetSession.R
+++ encoder/tests/fixEccConCutOnNotSingleFile/compareSetSession.R
@@ -3,6 +3,7 @@ CROSSVALIDATESMOOTH=0
 g=9.81
 source("/home/xavier/informatica/progs_meus/chronojump/encoder/util.R")
 library("pspline")
+library("signal")
 
 #this data is from mastercede session
 serie <- scan("serie.txt", skip=5)
@@ -15,6 +16,12 @@ print(sessio)
 
 speedSerie <- getSpeed(serie, .7)
 speedSessio <- getSpeed(sessio, .7)
+#speedSerie <- getSpeed(serie, .5)
+#speedSessio <- getSpeed(sessio, .5)
+#speedSerie <- getSpeedSplineUsingWeights(serie, .7)
+#speedSessio <- getSpeedSplineUsingWeights(sessio, .7)
+#speedSerie <- getSpeedTrimmingZerosInitialAndEnd(serie, .7)
+#speedSessio <- getSpeedTrimmingZerosInitialAndEnd(sessio, .7)
 
 accelSerie <- getAccelerationSafe(speedSerie)
 accelSessio <- getAccelerationSafe(speedSessio)
@@ -41,20 +48,21 @@ maxSpeedTInConcentricSessio = maxSpeedTSessio
 propulsiveEndSerie = findPropulsiveEnd(accelSerie$y, concentricSerie, maxSpeedTInConcentricSerie, "c", -1, -1, 72, 40, 0)
 propulsiveEndSessio = findPropulsiveEnd(accelSessio$y, concentricSessio, maxSpeedTInConcentricSessio, "c", -1, -1, 72, 40, 0)
 
-png ("compareSetSession.png", width=1920, height=1080)
+#png ("compareSetSession.png", width=1920, height=1080)
+png ("compareSetSession.png", width=1920, height=6080)
 plot (cumsum(serie), type="l", col="black", lty=1)
 lines (cumsum(sessio), col="black", lty=2)
 
 par(new=T)
-plot(speedSerie$y, type="l", col="green", lty=1)
-lines(speedSessio$y, col="green", lty=2)
+plot(speedSerie$x, speedSerie$y, type="l", col="green", lty=1)
+lines((2+speedSessio$x), speedSessio$y, col="green", lty=2)
 
 abline (v=propulsiveEndSerie, lty=1)
-abline (v=propulsiveEndSessio, lty=2)
+abline (v=2+propulsiveEndSessio, lty=2)
 
 par(new=T)
-plot(accelSerie$y, type="l", col="magenta", lty=1)
-lines(accelSessio$y, col="magenta", lty=2)
+plot(accelSerie$x, accelSerie$y, type="l", col="magenta", lty=1)
+lines((2+accelSessio$x), accelSessio$y, col="magenta", lty=2)
 
 #Serie
 dynamicsSerie = getDynamics("c",
@@ -68,6 +76,7 @@ startSerie <- 1
 endSerie <- length(speedSerie$y)
 endSerie <- propulsiveEndSerie
 print (mean(powerSerie[startSerie:endSerie]))
+#print (mean(powerSerie[(startSerie+2):endSerie]))
 
 #Sessio
 dynamicsSessio = getDynamics("c",
@@ -83,7 +92,16 @@ endSessio <- propulsiveEndSessio
 print (mean(powerSessio[startSessio:endSessio]))
 
 par(new=T)
-plot(powerSerie, type="l", col="red", lty=1)
-lines(powerSessio, col="red", lty=2)
+plot(1:length(powerSerie), powerSerie, type="l", col="red", lty=1, axes=F)
+#plot(1:length(powerSerie), powerSerie, type="l", col="red", lty=1, axes=F, ylim=c(0,100))
+#plot(1:length(powerSerie), powerSerie, type="l", col="red", lty=1, axes=F, ylim=c(1500,2500))
+abline(h=mean (powerSerie[startSerie:endSerie]), col="red")
+axis(4)
+lines(3:(length(powerSessio)+2), powerSessio, col="red", lty=2)
+abline(h=mean (powerSessio[startSessio:endSessio]), col="red", lty=2)
 dev.off()
 
+#print ("powerSerie")
+#print (powerSerie)
+#print ("powerSessio")
+#print (powerSessio)

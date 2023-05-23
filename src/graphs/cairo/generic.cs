85e151c57ddced87619500a64cfa861c5497b020
Xavier de Blas <xaviblas@gmail.com>
2023-05-23 12:23:57 +0200

    Fixed possibly crash on decimal too short or large on Cairo graphs getGridStepAndBoundaries

--- src/graphs/cairo/generic.cs
+++ src/graphs/cairo/generic.cs
@@ -164,12 +164,16 @@ public abstract class CairoGeneric
 	protected enum gridTypes { BOTH, HORIZONTALLINES, HORIZONTALLINESATRIGHT, VERTICALLINES }
 	protected void paintGridNiceAutoValues (Cairo.Context g, double minX, double maxX, double minY, double maxY, int seps, gridTypes gridType, int shiftRight, int fontH)
 	{
-		var gridXTuple = getGridStepAndBoundaries ((decimal) minX, (decimal) maxX, seps);
-		var gridYTuple = getGridStepAndBoundaries ((decimal) minY, (decimal) maxY, seps);
+		bool errorX;
+		bool errorY;
+
+		var gridXTuple = getGridStepAndBoundaries ((decimal) minX, (decimal) maxX, seps, out errorX);
+		var gridYTuple = getGridStepAndBoundaries ((decimal) minY, (decimal) maxY, seps, out errorY);
 
 		g.Save();
 		g.SetDash(new double[]{1, 2}, 0);
-		if(gridType == gridTypes.BOTH || gridType == gridTypes.VERTICALLINES)
+
+		if (! errorX && (gridType == gridTypes.BOTH || gridType == gridTypes.VERTICALLINES))
 			for(double i = gridXTuple.Item1; i <= gridXTuple.Item2 ; i += gridXTuple.Item3)
 			{
 				int xtemp = Convert.ToInt32(calculatePaintX(i));
@@ -179,7 +183,7 @@ public abstract class CairoGeneric
 				paintVerticalGridLine(g, xtemp, Util.TrimDecimals(i, 2), fontH);
 			}
 
-		if(gridType == gridTypes.BOTH || gridType == gridTypes.HORIZONTALLINES || gridType == gridTypes.HORIZONTALLINESATRIGHT)
+		if (! errorY && (gridType == gridTypes.BOTH || gridType == gridTypes.HORIZONTALLINES || gridType == gridTypes.HORIZONTALLINESATRIGHT))
 			for(double i = gridYTuple.Item1; i <= gridYTuple.Item2 ; i += gridYTuple.Item3)
 			{
 				int ytemp = Convert.ToInt32(calculatePaintY(i));
@@ -189,6 +193,7 @@ public abstract class CairoGeneric
 				paintHorizontalGridLine (g, ytemp, Util.TrimDecimals(i, 2), fontH,
 						(gridType == gridTypes.HORIZONTALLINESATRIGHT), shiftRight);
 			}
+
 		g.Stroke ();
 		g.Restore();
 	}
@@ -433,7 +438,7 @@ public abstract class CairoGeneric
 	 * thanks to: Andrew
 	 */
 	//private static Tuple<decimal, decimal, decimal> getGridStepAndBoundaries (decimal min, decimal max, int stepCount)
-	private static Tuple<double, double, double> getGridStepAndBoundaries (decimal min, decimal max, int stepCount)
+	private static Tuple<double, double, double> getGridStepAndBoundaries (decimal min, decimal max, int stepCount, out bool error)
 	{
 		// Minimal increment to avoid round extreme values to be on the edge of the chart
 		LogB.Information (string.Format ("max: {0}, min: {1}", max, min)); //atencio pq el proper potser peta i caldria posar try/catch i retornar Tupla en blanc si cal
@@ -452,6 +457,13 @@ public abstract class CairoGeneric
 		// Or use these if you prefer:  { 1, 2, 5, 10 };
 
 		// Normalize rough step to find the normalized one that fits best
+		double stepPowerDouble = (double)Math.Pow(10, -Math.Floor(Math.Log10((double)Math.Abs(roughStep))));
+		if (MathUtil.DecimalTooShortOrLarge (stepPowerDouble))
+		{
+			error = true;
+			return new Tuple<double, double, double>(0, 0, 0);
+		}
+
 		decimal stepPower = (decimal)Math.Pow(10, -Math.Floor(Math.Log10((double)Math.Abs(roughStep))));
 		var normalizedStep = roughStep * stepPower;
 
@@ -467,6 +479,7 @@ public abstract class CairoGeneric
 		decimal scaleMax = Math.Ceiling(max / step) * step;
 		decimal scaleMin = Math.Floor(min / step) * step;
 
+		error = false;
 		//return new Tuple<decimal, decimal, decimal>(scaleMin, scaleMax, step);
 		return new Tuple<double, double, double>(Convert.ToDouble(scaleMin), Convert.ToDouble(scaleMax), Convert.ToDouble(step));
 	}
--- src/util/math.cs
+++ src/util/math.cs
@@ -1289,6 +1289,10 @@ public static class MathUtil
 		return UtilAll.DivideSafe(d, max);
 	}
 
+	public static bool DecimalTooShortOrLarge (double d)
+	{
+		return (d < (double)decimal.MinValue || d > (double)decimal.MaxValue);
+	}
 }
 
 public class InterpolateSignal

52123cd9820e3b4cdc1a33225a09d41ccb2d692d
Xavier de Blas <xaviblas@gmail.com>
2023-05-30 15:23:22 +0200

    Minor change on FTD2XX_NET.cs to be able to compile

--- src/FTD2XX_NET.cs
+++ src/FTD2XX_NET.cs
@@ -27,7 +27,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Runtime.InteropServices;
 using System.Threading;
-using System.Windows.Forms;
+//using System.Windows.Forms;
 using System.IO;
 
 

2d4afe6fbda6d6e3db142c998f9881873fc2e676
Joeries Young <zofx@9icode.com>
2023-12-03 14:56:38 +0800

    Stuffs to make windows packages.

---
+++ src/MyPixbuf.cs
@@ -0,0 +1,53 @@
+﻿using Gdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chronojump
+{
+    /// <summary>
+    /// A custom class MyPixbuf to ignore images issues
+    /// </summary>
+    internal static class MyPixbuf
+    {
+        /// <summary>
+        /// Get Pixbuf by assembly name and resource path
+        /// </summary>
+        /// <param name="assembly">Assembly name</param>
+        /// <param name="resource">Resource path</param>
+        /// <returns>Return null if doesn't exist</returns>
+        public static Pixbuf Get(Assembly assembly, string resource)
+        {
+            Pixbuf pixbuf = null;
+            try
+            {
+                pixbuf = new Pixbuf(assembly, resource);
+            }
+            catch
+            {
+            }
+            return pixbuf;
+        }
+
+        /// <summary>
+        /// Get Pixbuf by file name
+        /// </summary>
+        /// <param name="filename">File name</param>
+        /// <returns>Return null if doesn't exist</returns>
+        public static Pixbuf Get(string filename)
+        {
+            Pixbuf pixbuf = null;
+            try
+            {
+                pixbuf = new Pixbuf(filename);
+            }
+            catch
+            {
+            }
+            return pixbuf;
+        }
+    }
+}
---
+++ win32/gtk3/gtk3-runtime/gtk3r-env.bat
@@ -0,0 +1,9 @@
+@set GTK3R_PREFIX=C:\Program Files\GTK3-Runtime Win64
+
+@echo Setting environment variables for GTK3-Runtime.
+@echo.
+
+@echo set PATH=%GTK3R_PREFIX%;%%PATH%%
+@set PATH=%GTK3R_PREFIX%;%PATH%
+
+@echo.

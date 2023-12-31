7c2b492ceabac405833ba58a89be894a8a8e5e84
Joeries Young <zofx@9icode.com>
2023-12-31 15:51:53 +0800

    Modify packaging scripts to exit while there are compiling errors.

--- win32/package-x64.bat
+++ win32/package-x64.bat
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd1631bd0249b1fc6926eb6dde4124c8572968239d87a2ddbaad19f25da98494
-size 328
+oid sha256:d69b43fac31c586065c376f7641c6c48f1cf407f7d47e5c1f88aeaa7592466f2
+size 373

08208a2d88a63e75cf2c276019b206973940ac07
Xavier de Blas <xaviblas@gmail.com>
2024-02-19 11:06:27 +0100

    Updated logchronojump.bat

--- win32/deps/bin/logchronojump.bat
+++ win32/deps/bin/logchronojump.bat
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef3a313908730903fbcc9d939e35db8989cd5ca7dd092d1c7dfa0c5c5a8b1f78
-size 487
+oid sha256:0d9f720fa8d3a62586e970a1de1540baa4955671b1d95c2dc7af84e1b08e15f1
+size 471

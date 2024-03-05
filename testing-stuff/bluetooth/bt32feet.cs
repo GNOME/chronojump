870f09356825da98ac2f3d795848187e81a05d2d
Xavier de Blas <xaviblas@gmail.com>
2024-03-05 13:56:19 +0100

    Fixed bt32feet example

--- testing-stuff/bluetooth/bt32feet.cs
+++ testing-stuff/bluetooth/bt32feet.cs
@@ -2,7 +2,7 @@
 //https://www.c-sharpcorner.com/article/transferring-data-via-bluetooth-in-net/
 //but it uses legacy (old): dotnet add package 32feet.NET
 
-//fix below code to work with:
+//fixed below code to work with:
 //https://www.nuget.org/packages/InTheHand.Net.Bluetooth
 //new: dotnet add package InTheHand.Net.Bluetooth --version 4.1.40
 
@@ -17,7 +17,8 @@ class Program
         // Discover nearby Bluetooth devices
 
         BluetoothClient bluetoothClient = new BluetoothClient();
-        BluetoothDeviceInfo[] devices = bluetoothClient.DiscoverDevices();
+        //BluetoothDeviceInfo[] devices = bluetoothClient.DiscoverDevices();
+        System.Collections.Generic.IReadOnlyCollection<InTheHand.Net.Sockets.BluetoothDeviceInfo> devices = bluetoothClient.DiscoverDevices();
 
         Console.WriteLine("Discovered Bluetooth Devices:");
         foreach (BluetoothDeviceInfo device in devices)
@@ -28,10 +29,17 @@ class Program
             Console.WriteLine();
         }
 
+
+
+
         //   output Device Address : 8803E9C06F34;
         //  Convert this to 88:03:E9:C0:6F:34
 
-        BluetoothAddress deviceAddress = BluetoothAddress.Parse("88:03:E9:C0:6F:34");
+        //BluetoothAddress deviceAddress = BluetoothAddress.Parse("88:03:E9:C0:6F:34");
+        //BluetoothAddress deviceAddress = BluetoothAddress.Parse("24753AD3DB6E");
+        //BluetoothAddress deviceAddress = BluetoothAddress.Parse("24:75:3A:D3:DB:6E");
+        BluetoothAddress deviceAddress = BluetoothAddress.Parse("DE:33:46:1D:AF:0B");
+
         BluetoothEndPoint endPoint = new BluetoothEndPoint(deviceAddress, BluetoothService.SerialPort);
         BluetoothClient client = new BluetoothClient();
 

3ac23e2271dc56e92b1cbb5e2313fbed9b65739e
Xavier de Blas <xaviblas@gmail.com>
2024-04-24 16:10:09 +0200

    At cloudRead station do not allow to show persons on top

--- src/gui/app1/chronojumpPersons.cs
+++ src/gui/app1/chronojumpPersons.cs
@@ -42,6 +42,10 @@ public partial class ChronoJumpWindow
 
 	private void showPersonsOnTop (bool onTop)
 	{
+		//do not allow on top when using other dbs
+		if (configChronojump.CanOpenExternalDB || configChronojump.ReadFromCloudMainPath != "")
+			onTop = false;
+
 		hbox_top_person.Visible = onTop;
 		hbox_top_person_encoder.Visible = onTop;
 

ff31e9d4babf5c19cb438ef922f7c9052d6f6097
Xavier de Blas <xaviblas@gmail.com>
2024-07-01 16:18:59 +0200

    fixCutDiffSignalReps with new getRepetitions code

--- encoder/tests/fixCutDiffSignalReps/fix.R
+++ encoder/tests/fixCutDiffSignalReps/fix.R
@@ -1,10 +1,9 @@
-#source("/home/xavier/informatica/progs_meus/chronojump/encoder/util.R")
 
 disSerie <- scan("1569-2024-02-26_11-37-20.txt", sep=",")
 disRep <- scan("chronojump_enc_curve_7.txt", sep=",")
 
-disSerie <- disSerie[!is.na(serie)]
-disRep <- disRep[!is.na(rep)]
+disSerie <- disSerie[!is.na(disSerie)]
+disRep <- disRep[!is.na(disRep)]
 
 posSerie = cumsum(disSerie)
 posRep = cumsum(disRep)
@@ -21,4 +20,19 @@ lines (repStart:(repStart+length(posRep)-1),posRep+107, col="red")
 
 abline (v=c(1+repStart, 967+repStart, 1336+repStart, 3700+repStart), col="red", lty=3)
 
+source("/home/xavier/informatica/progs_meus/chronojump/encoder/util.R")
 
+conMinDisplacement = 10
+eccMinDisplacement = 10
+
+par(mfrow=c(2,1))
+plot (posSerie, type="l", main="concentric")
+repsForEccon_l = getRepetitionsForEccon (posSerie, "c", conMinDisplacement, eccMinDisplacement)
+abline (v=repsForEccon_l$repStart)
+abline (v=repsForEccon_l$repEnd, col="red")
+
+plot (posSerie, type="l", main="ec")
+repsForEccon_l = getRepetitionsForEccon (posSerie, "ec", conMinDisplacement, eccMinDisplacement)
+abline (v=repsForEccon_l$repStart)
+abline (v=repsForEccon_l$repEnd, col="red")
+par(mfrow=c(1,1))

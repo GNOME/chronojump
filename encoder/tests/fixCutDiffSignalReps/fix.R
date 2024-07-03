58716a542e07d83beaf03490f19a6688234e88b5
Xavier de Blas <xaviblas@gmail.com>
2024-07-03 11:15:53 +0200

    fixCutDiffSignalReps/fix.R perfect!

--- encoder/tests/fixCutDiffSignalReps/fix.R
+++ encoder/tests/fixCutDiffSignalReps/fix.R
@@ -68,22 +68,39 @@ curves <- getRepsLikeFindCurvesNew (disSerie, "ecS", FALSE, 10)
 print (curves)
 
 png ("fix.png", width=1920, height=1080)
-plot (posSerie, type="l")
+plot (posSerie, type="l", ylim = c(min(posSerie) -20, max(posSerie)+15), main="green should be = blue")
+legend("topright",
+       col=c("green", "red", "black", "blue", "brown"),
+       lty=1,
+       legend=c(
+		"As set reduced ecc, con",
+		"As set not-reduced ecc, con",
+		"Full set",
+		"As reps reduced ecc, con",
+		"Full rep ecc, con")
+)
+
 for (i in 1:length(curves[,1]))
 {
 	# "ecS"
 	displacementTemp = disSerie[curves[i,1]:curves[i,2]]
 	posTemp = cumsum (displacementTemp)
-	lines (curves[i,1]:(curves[i,1]+length(posTemp)-1), posTemp+posSerie[curves[i,1]]+5, col="red")
 
 	reducedCurve_l <- NULL
+	ySpace <- NULL
 	if (posSerie[curves[i,1]] < posSerie[curves[i,2]])
+	{
 		reducedCurve_l <- reduceCurveByPredictStartEnd (displacementTemp, "c", 10)
-	else
+		ySpace = 5
+	} else {
 		reducedCurve_l <- reduceCurveByPredictStartEnd (displacementTemp, "e", 10)
+		ySpace = 10
+	}
+	
+	lines (curves[i,1]:(curves[i,1]+length(posTemp)-1), posTemp+posSerie[curves[i,1]] + ySpace, col="red")
 
-	print ("reducedCurve_l startPos, endPos")
-	print (c(reducedCurve_l$startPos, reducedCurve_l$endPos))
+	#print ("reducedCurve_l startPos, endPos")
+	#print (c(reducedCurve_l$startPos, reducedCurve_l$endPos))
 
 	curves[i,2] <- curves[i,1] + (reducedCurve_l$endPos -1)
 	curves[i,1] <- curves[i,1] + (reducedCurve_l$startPos -1)
@@ -91,7 +108,7 @@ for (i in 1:length(curves[,1]))
 	displacementTempReduced = disSerie[curves[i,1]:curves[i,2]]
 	posTempReduced = cumsum (displacementTempReduced)
 
-	lines (curves[i,1]:(curves[i,1]+length(posTempReduced)-1), posTempReduced+posSerie[curves[i,1]]+10, col="green")
+	lines (curves[i,1]:(curves[i,1]+length(posTempReduced)-1), posTempReduced+posSerie[curves[i,1]]+15, col="green")
 }
 
 #2) now as reps
@@ -99,35 +116,58 @@ graphRep <- function (displRep, xPlotStart, yPlotStartE, yPlotStartC)
 {
 	posRep = cumsum (displRep)
 
-	endEcc = mean(which(posRep == min(posRep)))
-	startCon = mean(which(posRep == min(posRep)))
+	#print (displRep)
+	#print (posRep)
+	
+	#put abline in the middle
+	abline (v=xPlotStart + mean(which(posRep == min(posRep))))
 
-	print (c("**startCon**", startCon))
-	abline (v=xPlotStart + startCon)
-	ecS_ecc_l <- reduceCurveByPredictStartEnd (posRep[1:endEcc], "e", 10)
-	ecS_con_l <- reduceCurveByPredictStartEnd (posRep[startCon:length(posRep)], "c", 10)
+	#endEcc = mean(which(posRep == min(posRep)))
+	endEcc = max(which(posRep == min(posRep)))
+	#print (paste("endEcc", endEcc))
+	endEccDispl = endEcc -1
+	if (endEccDispl < 1)
+		endEccDispl = 1
+
+	#startCon = mean(which(posRep == min(posRep)))
+	startCon = min(which(posRep == min(posRep))) #this is pos, make it displ
+	#print (paste("startCon", startCon))
+	startConDispl = startCon -1
+	if (startConDispl < 1)
+		startConDispl = 1
+
+	ePos = posRep [1:endEcc]
+	lines (xPlotStart + 1:endEcc, yPlotStartE + ePos -15, col="brown")
+	cPos = posRep [startCon:length(posRep)]
+	lines (xPlotStart + startCon:(startCon+length(cPos)-1), yPlotStartC + cPos -20, col="brown")
+
+	#print (c("**startCon**", startCon))
+	#print (c("**length Con**", length(posRep)-startCon))
+	ecS_ecc_l <- reduceCurveByPredictStartEnd (displRep[1:endEccDispl], "e", 10)
+	ecS_con_l <- reduceCurveByPredictStartEnd (displRep[startConDispl:length(displRep)], "c", 10)
 
 	ecS_ecc_l$startPos = 1
-	ecS_con_l$endPos = length(posRep) - startCon
+	ecS_con_l$endPos = length(posRep)
 
+	#+1 to be pos
 	eStart = ecS_ecc_l$startPos
-	eEnd = ecS_ecc_l$endPos
-	cStart = ecS_con_l$startPos + startCon #TODO: check that graph.R has this
-	cEnd = ecS_con_l$endPos + startCon #TODO: same as above
-	print (c("eStart", eStart))
-	print (c("eEnd", eEnd))
-	print (c("cStart", cStart))
-	print (c("cEnd", cEnd))
-	#print ("eStart, eEnd, cStart, cEnd", eStart, eEnd, cStart, cEnd)
+	eEnd = ecS_ecc_l$endPos + 1
+	cStart = ecS_con_l$startPos + startCon + 1#TODO: check that graph.R has thi]s
+	cEnd = ecS_con_l$endPos + startCon +1 #TODO: same as above
+	#print (c("eStart", eStart))
+	#print (c("eEnd", eEnd))
+	#print (c("cStart", cStart))
+	#print (c("cEnd", cEnd))
 
 	ePos = posRep [eStart:eEnd]
 	cPos = posRep [cStart:cEnd]
 	lines (xPlotStart + eStart:(eStart+length(ePos)-1), yPlotStartE + ePos -5, col="blue")
-	lines (xPlotStart + cStart:(cStart+length(cPos)-1), yPlotStartC + cPos -10, col="brown")
+	lines (xPlotStart + cStart:(cStart+length(cPos)-1), yPlotStartC + cPos -10, col="blue")
 }
 graphRep (disRep6, curves[1,1], posSerie[curves[1,1]], posSerie[curves[1,1]])
 graphRep (disRep7, curves[3,1], posSerie[curves[3,1]], posSerie[curves[3,1]])
 graphRep (disRep8, curves[5,1], posSerie[curves[5,1]], posSerie[curves[5,1]])
+	
 dev.off ()
 
 

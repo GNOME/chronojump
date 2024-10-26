7d82e060939d54a1f0cc115e82f41822c5562656
Joeries Young <joeries.young@gmail.com>
2024-10-26 15:38:56 +0800

    Fix SQLite dll files issue on Debian.

--- src/post-build-linux.sh
+++ src/post-build-linux.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 cd $(dirname "$0")
 rm -rf ../package/linux/deps/share/chronojump/encoder/
 rm -rf ../package/linux/deps/share/chronojump/r-scripts/
@@ -28,3 +30,34 @@ rm -rf "$1/share/doc/chronojump/Makefile"
 cp "../images/bad.wav" "$1/share/chronojump/images/bad.wav"
 cp "../images/ok.wav" "$1/share/chronojump/images/ok.wav"
 cp "../images/start.wav" "$1/share/chronojump/images/start.wav"
+
+#Get OS Name
+OS=$(echo $(uname -a) | tr [:upper:] [:lower:])
+if [[ "$OS" =~ "debian" ]]; then  
+    OS="debian"
+else
+    OS=""
+fi
+
+#Get OS Name
+ARCH="$(uname -m)"
+if [ "$ARCH" == "x86_64" ]; then  
+    ARCH="x64"
+elif [ "$ARCH" == "armv7l" ]; then  
+    ARCH="arm"
+elif [ "$ARCH" == "aarch64" ]; then  
+    ARCH="arm64"
+else
+    ARCH=""
+fi
+
+#Get Major Version Number
+. /etc/os-release    
+VERSION="$(echo $VERSION_ID | sed 's/\..*//')"
+
+#Copy if existing
+if [ -e "../package/linux/refs/runtimes/$OS.$VERSION-$ARCH/native/SQLite.Interop.dll" ]; then
+    cp "../package/linux/refs/System.Data.SQLite.dll" "$1/System.Data.SQLite.dll"
+    cp "../package/linux/refs/System.Data.SQLite.dll.config" "$1/System.Data.SQLite.dll.config"
+    cp "../package/linux/refs/runtimes/$OS.$VERSION-$ARCH/native/SQLite.Interop.dll" "$1/SQLite.Interop.dll"
+fi

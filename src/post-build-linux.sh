249774853d0f1f6ce47c5a0434dcb69b85a153ad
Xavier de Blas <xaviblas@gmail.com>
2024-10-28 08:10:37 +0100

    Last post-build-linux.sh from joeries

--- src/post-build-linux.sh
+++ src/post-build-linux.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 set -e
 
+error_exit()
+{
+    echo "$1" >&2
+    exit 1
+}
+
 cd $(dirname "$0")
 rm -rf ../package/linux/deps/share/chronojump/encoder/
 rm -rf ../package/linux/deps/share/chronojump/r-scripts/
-cp -r $(find ../package/linux/deps/ -mindepth 1 -maxdepth 1 -type d) "$1/"
 rm -rf "$1/bin/"
-mkdir -p "$1/bin/"
+rm -rf "$1/share/"
+#mkdir -p "$1/bin/"
+#cp -r $(find ../package/linux/deps/ -mindepth 1 -maxdepth 1 -type d) "$1/"
+cp -r ../package/linux/deps/bin "$1"
+cp -r ../package/linux/deps/share "$1"
 cp -r "./angle/" "$1/bin/angle/"
 cp -r "./chronojump-importer/" "$1/bin/chronojump-importer/"
-rm -rf "$1/share/"
 mkdir -p "$1/share/chronojump/images/"
 cp -r "../win32/chronojump_icon.ico" "$1/share/chronojump/images/chronojump_icon.ico"
 cp -r "../encoder/" "$1/share/chronojump/encoder/"
@@ -20,10 +28,6 @@ rm -rf "$1/bin/chronojump-importer/Makefile"
 rm -rf "$1/bin/encoder/Makefile.in"
 rm -rf "$1/bin/encoder/Makefile.am"
 rm -rf "$1/bin/encoder/Makefile"
-rm -rf "$1/po/Makefile.in.in"
-rm -rf "$1/po/Makefile.in"
-rm -rf "$1/po/Makefile.am"
-rm -rf "$1/po/Makefile"
 rm -rf "$1/share/doc/chronojump/Makefile.in"
 rm -rf "$1/share/doc/chronojump/Makefile.am"
 rm -rf "$1/share/doc/chronojump/Makefile"
@@ -32,27 +36,26 @@ cp "../images/ok.wav" "$1/share/chronojump/images/ok.wav"
 cp "../images/start.wav" "$1/share/chronojump/images/start.wav"
 
 #Get OS Name
-OS=$(echo $(uname -a) | tr [:upper:] [:lower:])
-if [[ "$OS" =~ "debian" ]]; then  
+UNAME_ALL=$(echo $(uname -a) | tr [:upper:] [:lower:])
+OS=""
+if [[ "$UNAME_ALL" =~ "debian" ]]; then  
     OS="debian"
-else
-    OS=""
 fi
 
 #Get Arch Name
 ARCH="$(uname -m)"
-if [ "$ARCH" == "x86_64" ]; then  
+if [ "$ARCH" = "x86_64" ]; then
     ARCH="x64"
-elif [ "$ARCH" == "armv7l" ]; then  
+elif [ "$ARCH" = "armv7l" ]; then
     ARCH="arm"
-elif [ "$ARCH" == "aarch64" ]; then  
+elif [ "$ARCH" = "aarch64" ]; then
     ARCH="arm64"
 else
     ARCH=""
 fi
 
 #Get Major Version Number
-. /etc/os-release    
+. /etc/os-release
 VERSION="$(echo $VERSION_ID | sed 's/\..*//')"
 
 #Copy if existing
@@ -61,3 +64,4 @@ if [ -e "../package/linux/refs/runtimes/$OS.$VERSION-$ARCH/native/SQLite.Interop
     cp "../package/linux/refs/System.Data.SQLite.dll.config" "$1/System.Data.SQLite.dll.config"
     cp "../package/linux/refs/runtimes/$OS.$VERSION-$ARCH/native/SQLite.Interop.dll" "$1/SQLite.Interop.dll"
 fi
+

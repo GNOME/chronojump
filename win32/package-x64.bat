04ff1a8ff68097a7df9f750812a2acb95e0e13ee
Joeries Young <joeries.young@gmail.com>
2025-01-09 12:13:30 +0800

    Enable making arm/arm64 packages.

--- win32/package-x64.bat
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d513792b1094c1820f6d3a357b189c91cbef25e73c3ede1bb0151466c10aa5f6
-size 221
---
+++ win32/package.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eb1f36ab504c0135792385c8974db1235d3675f374ae175ae142fd343ca623ef
+size 350

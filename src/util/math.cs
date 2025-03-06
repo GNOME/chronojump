e88ef2f28bbfb55827a3b4822120ca644717acab
Xavier de Blas <xaviblas@gmail.com>
2025-03-06 21:07:37 +0100

    Method DoubleCanBeInt32Safe : check double can be int (by boundaries)

--- src/util/math.cs
+++ src/util/math.cs
@@ -1686,6 +1686,23 @@ public static class MathUtil
 	{
 		return (d < (double)decimal.MinValue || d > (double)decimal.MaxValue);
 	}
+
+	//thanks to: https://stackoverflow.com/a/4939192 cc by-sa 2.5
+	public static bool DoubleCanBeInt32Safe (double value)
+	{
+		const double Min = int.MinValue - 0.5;
+		const double Max = int.MaxValue + 0.5;
+
+		// Notes:
+		// 1. double.IsNaN is needed for exclusion purposes because NaN compares
+		//    false for <, >=, etc. for every value (including itself).
+		// 2. value < Min is correct because -2147483648.5 rounds to int.MinValue.
+		// 3. value >= Max is correct because 2147483648.5 rounds to int.MaxValue + 1.
+		if (double.IsNaN(value) || value < Min || value >= Max)
+			return false;
+
+		return true;
+	}
 }
 
 public class InterpolateSignal

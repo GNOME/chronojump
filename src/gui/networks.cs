9640d998892ea3a7145915a68f59e997493f3c0e
Xavier de Blas <xaviblas@gmail.com>
2025-04-01 17:01:05 +0200

    Cont of last commit

--- src/gui/networks.cs
+++ src/gui/networks.cs
@@ -309,11 +309,6 @@ public partial class ChronoJumpWindow
 			image_encoder_inertial_instructions.Visible = false;
 		}
 
-		if(configChronojump.FourPlatforms)
-		{
-			changeModeCheckRadios (Constants.Modes.OTHER);
-		}
-
 		if(configChronojump.GuiTest)
 		{
 			button_menu_guiTest.Visible = true;

c385aee48c0e20d9cd8998ce0205b69343750d79
Xavier de Blas <xaviblas@gmail.com>
2025-04-15 10:31:41 +0200

    Added missing file

---
+++ src/gui/app1/treeviews/beepTest.cs
@@ -0,0 +1,73 @@
+/*
+ * This file is part of ChronoJump
+ *
+ * Chronojump is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or   
+ *    (at your option) any later version.
+ *    
+ * Chronojump is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ *    GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Copyright (C) 2025   Xavier de Blas <xaviblas@gmail.com>
+ */
+
+
+using System;
+using Gtk;
+//using Glade;
+using System.Collections; //ArrayList
+using System.Collections.Generic; //List
+
+public partial class ChronoJumpWindow 
+{
+	private void fillTreeView_beepTest (string filter) {
+		fillTreeView_beepTest (filter, false);
+	}
+	private void fillTreeView_beepTest (string filter, bool dbconOpened)
+	{
+		LogB.Information ("At fillTreeView_beepTest");
+		if (currentSession == null) {
+			/*
+			 * This happens when the user "Imports a session": Chronojump tries to
+			 * update comboboxes, it reaches here because the comboboxes are updated
+			 * But if the user didn't have any
+			 * open session currentSession variable (see below) is null and it crashed here
+			 * (when it did currentSession.UniqueID with currentSession==null)
+			 */
+			return;
+		}
+
+		/*
+		List<BeepTest> beepTest_l = SqliteBeepTest.Select (dbconOpened,
+				currentSession.UniqueID, currentPersonOrAll ()//,
+				//"", Sqlite.Orders_by.DEFAULT, 0);
+			);
+		string [] beepTestSA = TreeViewBeepTest.ListToStringArray (beepTest_l);
+		*/
+		SqliteTests sqliteTests = new SqliteBeepTest ();
+		string [] beepTestSA = sqliteTests.SelectSA (dbconOpened,
+				currentSession.UniqueID, currentPersonOrAll (),
+				//"",
+				false, "",
+				Sqlite.Orders_by.DEFAULT, 0);
+
+		if (current_mode == Constants.Modes.BEEPTEST)
+			treeViewResultsSession.Fill (beepTestSA,
+					filter,
+					//Util.GetVideosOfSessionAndMode (currentSession.UniqueID, Constants.TestTypes.BEEPTEST));
+					new List<string> ());
+
+		//if show just one person, have it expanded
+		if (! radio_contacts_results_personAll.Active && currentPerson != null)
+			treeview_results_session.ExpandAll();
+		else
+			expandOrMinimizeTreeView((TreeViewEvent) treeViewResultsSession, treeview_results_session);
+	}
+}

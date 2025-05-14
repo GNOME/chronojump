5d2d0f503bcc0069b1d1f1f8ecf080d3c479dc3b
Xavier de Blas <xaviblas@gmail.com>
2025-05-14 13:19:17 +0200

    Many upgrades to chronopic_test dialog (grid_iqa, back, icons, ...)

--- src/gui/chronopic_test.cs
+++ src/gui/chronopic_test.cs
@@ -31,37 +31,28 @@ public class ChronopicTestWindow
 	// at glade ---->
 	Gtk.Window chronopic_test;
 	Gtk.Notebook notebook;
-	Gtk.Button button_close;
 	Gtk.Box box_result;
 	Gtk.Box box_problems;
+	Gtk.Grid grid_iqa;
+	Gtk.Label label_instructions;
+	Gtk.Label label_question;
 	Gtk.Label label_all_ok;
 	Gtk.Label label_contact_reason;
+	Gtk.Button button_yes;
+	Gtk.Button button_no;
+	Gtk.Image image_yes;
+	Gtk.Image image_no;
+	Gtk.Button button_back;
+	Gtk.Image image_back;
+	Gtk.Button button_close;
 	Gtk.Image image_close;
 
-	//1st tab
+	// notebook tab images
 	Gtk.Image image_ledOn;
-	Gtk.Button button_ledOn_yes;
-	Gtk.Button button_ledOn_no;
-
-	//2nd tab
 	Gtk.Image image_testLed;
-	Gtk.Button button_testLed_yes;
-	Gtk.Button button_testLed_no;
-
-	//3rd tab
 	Gtk.Image image_rca;
-	Gtk.Button button_rca_yes;
-	Gtk.Button button_rca_no;
-
-	//4th tab
 	Gtk.Image image_platform_connect;
-	Gtk.Button button_platform_connect_yes;
-	Gtk.Button button_platform_connect_no;
-
-	//5th tab
 	Gtk.Image image_platform_stand;
-	Gtk.Button button_platform_stand_yes;
-	Gtk.Button button_platform_stand_no;
 
 	// <---- at glade
 	
@@ -84,8 +75,9 @@ public class ChronopicTestWindow
 		if(! Config.UseSystemColor)
 		{
 			UtilGtk.WindowColor (chronopic_test, Config.ColorBackground);
-			UtilGtk.ContrastLabelsNotebook (Config.ColorBackgroundIsDark, notebook);
+			//UtilGtk.ContrastLabelsNotebook (Config.ColorBackgroundIsDark, notebook);
 			UtilGtk.ContrastLabelsBox (Config.ColorBackgroundIsDark, box_result);
+			UtilGtk.ContrastLabelsGrid (Config.ColorBackgroundIsDark, grid_iqa);
 		}
 
 		chronopic_test.Parent = parent;
@@ -111,6 +103,9 @@ public class ChronopicTestWindow
 	
 	private void initialize()
 	{
+		image_yes.Pixbuf = Chronojump.MyPixbuf.Get(null, Util.GetImagePath(false) + "thumb_up.png");
+		image_no.Pixbuf = Chronojump.MyPixbuf.Get(null, Util.GetImagePath(false) + "thumb_down.png");
+		image_back.Pixbuf = Chronojump.MyPixbuf.Get(null, Util.GetImagePath(false) + "arrow_back.png");
 		image_close.Pixbuf = Chronojump.MyPixbuf.Get(null, Util.GetImagePath(false) + "image_close.png");
 
 		notebook.CurrentPage = 0;
@@ -129,86 +124,99 @@ public class ChronopicTestWindow
 		image_platform_stand.Pixbuf = pixbuf;
 	}
 
-	// yes/no butrons
-	// 1st tab
-	private void on_button_ledOn_yes_clicked (object o, EventArgs args)
+	private void changePage (int page)
 	{
-		notebook.CurrentPage = 1;
+		notebook.CurrentPage = page;
+		button_yes.Sensitive = true;
+		button_back.Sensitive = true;
+
+		switch (page)
+		{
+			case 0:
+				label_instructions.Text = Catalog.GetString ("Remove the RCA cable. Then, check LED D1.");
+				label_question.Text = Catalog.GetString ("Is the green LED D1 on?");
+				button_back.Sensitive = false;
+				break;
+			case 1:
+				label_instructions.Text = Catalog.GetString ("Press test button.");
+				label_question.Text = Catalog.GetString ("When test button is pressed, is the green LED D1 off?");
+				break;
+			case 2:
+				label_instructions.Text = Catalog.GetString ("Connect RCA cable.") + "\n" +
+					Catalog.GetString ("Touch the tip and teeth of RCA with a metallic object (like a key).");
+				label_question.Text = Catalog.GetString ("Is the green LED D1 off?");
+				break;
+			case 3:
+				label_instructions.Text = Catalog.GetString ("Connect Chronopic with the platform using the RCA.") + "\n" + 
+					Catalog.GetString ("Do not stand on the platform.");
+				label_question.Text = Catalog.GetString ("Is the green LED D1 on?");
+				break;
+			case 4:
+				label_instructions.Text = Catalog.GetString ("Stand on the platform.");
+				label_question.Text = Catalog.GetString ("Is the green LED D1 off?");
+				break;
+		}
 	}
-	private void on_button_ledOn_no_clicked (object o, EventArgs args)
+
+	private void on_button_yes_clicked (object o, EventArgs args)
 	{
-		label_contact_reason.Text = "Chronopic RCA cable removed. Led D1 is Off.";
+		if (notebook.CurrentPage == 4)
+		{
+			box_result.Visible = true;
+			box_problems.Visible = false;
+			label_all_ok.Visible = true;
+			button_yes.Sensitive = false;
+		}
+		else
+		{
+			box_result.Visible = false;
+			box_problems.Visible = false;
+			label_all_ok.Visible = false;
+			changePage (notebook.CurrentPage + 1);
+		}
+	}
+	
+	private void on_button_no_clicked (object o, EventArgs args)
+	{
+		// notebook.Sensitive = false; disabled to make the back work
 		box_result.Visible = true;
 		box_problems.Visible = true;
 		label_all_ok.Visible = false;
 
-		notebook.Sensitive = false;
+		switch (notebook.CurrentPage)
+		{
+			case 0:
+				label_contact_reason.Text = "Chronopic RCA cable removed. Led D1 is Off.";
+				break;
+			case 1:
+				label_contact_reason.Text = "Chronopic test button pressed, RCA is on.";
+				break;
+			case 2:
+				label_contact_reason.Text = "RCA cable is not working. Try to change it.";
+				break;
+			case 3:
+				label_contact_reason.Text = "Platform or Chronopic is not working properly.";
+				break;
+			case 4:
+				label_contact_reason.Text = "Plataform or Chronopic is not working properly.";
+				break;
+		}
 	}
 
-	// 2nd tab
-	private void on_button_testLed_yes_clicked (object o, EventArgs args)
+	private void on_button_back_clicked (object o, EventArgs args)
 	{
-		notebook.CurrentPage = 2;
-	}
-	private void on_button_testLed_no_clicked (object o, EventArgs args)
-	{
-		label_contact_reason.Text = "Chronopic test button pressed, RCA is on.";
-		box_result.Visible = true;
-		box_problems.Visible = true;
+		// impossible because is not sensitive, but just in case
+		if (notebook.CurrentPage == 0)
+			return;
+
+		changePage (notebook.CurrentPage - 1);
+		box_result.Visible = false;
+		//box_problems.Visible = false;
 		label_all_ok.Visible = false;
-
-		notebook.Sensitive = false;
 	}
 
-	// 3rd tab
-	private void on_button_rca_yes_clicked (object o, EventArgs args)
+	public void Close()
 	{
-		notebook.CurrentPage = 3;
-	}
-	private void on_button_rca_no_clicked (object o, EventArgs args)
-	{
-		label_contact_reason.Text = "RCA cable is not working. Try to change it.";
-		box_result.Visible = true;
-		box_problems.Visible = true;
-		label_all_ok.Visible = false;
-
-		notebook.Sensitive = false;
-	}
-
-	// 4th tab
-	private void on_button_platform_connect_yes_clicked (object o, EventArgs args)
-	{
-		notebook.CurrentPage = 4;
-	}
-	private void on_button_platform_connect_no_clicked (object o, EventArgs args)
-	{
-		label_contact_reason.Text = "Platform or Chronopic is not working properly.";
-		box_result.Visible = true;
-		box_problems.Visible = true;
-		label_all_ok.Visible = false;
-
-		notebook.Sensitive = false;
-	}
-
-	// 5th tab
-	private void on_button_platform_stand_yes_clicked (object o, EventArgs args)
-	{
-		box_result.Visible = true;
-		box_problems.Visible = false;
-		label_all_ok.Visible = true;
-	}
-	private void on_button_platform_stand_no_clicked (object o, EventArgs args)
-	{
-		label_contact_reason.Text = "Plataform or Chronopic is not working properly.";
-		box_result.Visible = true;
-		box_problems.Visible = true;
-		label_all_ok.Visible = false;
-
-		notebook.Sensitive = false;
-	}
-
-
-	public void Close() {
 		on_button_close_clicked (new object (), new EventArgs ());
 	}
 
@@ -228,37 +236,28 @@ public class ChronopicTestWindow
 	{
 		chronopic_test = (Gtk.Window) builder.GetObject ("chronopic_test");
 		notebook = (Gtk.Notebook) builder.GetObject ("notebook");
-		button_close = (Gtk.Button) builder.GetObject ("button_close");
 		box_result = (Gtk.Box) builder.GetObject ("box_result");
 		box_problems = (Gtk.Box) builder.GetObject ("box_problems");
+		grid_iqa = (Gtk.Grid) builder.GetObject ("grid_iqa");
+		label_instructions = (Gtk.Label) builder.GetObject ("label_instructions");
+		label_question = (Gtk.Label) builder.GetObject ("label_question");
 		label_all_ok = (Gtk.Label) builder.GetObject ("label_all_ok");
 		label_contact_reason = (Gtk.Label) builder.GetObject ("label_contact_reason");
+		button_yes = (Gtk.Button) builder.GetObject ("button_yes");
+		button_no = (Gtk.Button) builder.GetObject ("button_no");
+		image_yes = (Gtk.Image) builder.GetObject ("image_yes");
+		image_no = (Gtk.Image) builder.GetObject ("image_no");
+		button_back = (Gtk.Button) builder.GetObject ("button_back");
+		image_back = (Gtk.Image) builder.GetObject ("image_back");
+		button_close = (Gtk.Button) builder.GetObject ("button_close");
 		image_close = (Gtk.Image) builder.GetObject ("image_close");
 
-		//1st tab
+		// notebook tab images
 		image_ledOn = (Gtk.Image) builder.GetObject ("image_ledOn");
-		button_ledOn_yes = (Gtk.Button) builder.GetObject ("button_ledOn_yes");
-		button_ledOn_no = (Gtk.Button) builder.GetObject ("button_ledOn_no");
-
-		//2nd tab
 		image_testLed = (Gtk.Image) builder.GetObject ("image_testLed");
-		button_testLed_yes = (Gtk.Button) builder.GetObject ("button_testLed_yes");
-		button_testLed_no = (Gtk.Button) builder.GetObject ("button_testLed_no");
-
-		//3rd tab
 		image_rca = (Gtk.Image) builder.GetObject ("image_rca");
-		button_rca_yes = (Gtk.Button) builder.GetObject ("button_rca_yes");
-		button_rca_no = (Gtk.Button) builder.GetObject ("button_rca_no");
-
-		//4th tab
 		image_platform_connect = (Gtk.Image) builder.GetObject ("image_platform_connect");
-		button_platform_connect_yes = (Gtk.Button) builder.GetObject ("button_platform_connect_yes");
-		button_platform_connect_no = (Gtk.Button) builder.GetObject ("button_platform_connect_no");
-
-		//5th tab
 		image_platform_stand = (Gtk.Image) builder.GetObject ("image_platform_stand");
-		button_platform_stand_yes = (Gtk.Button) builder.GetObject ("button_platform_stand_yes");
-		button_platform_stand_no = (Gtk.Button) builder.GetObject ("button_platform_stand_no");
 	}
 }
 

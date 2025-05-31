ca7b25ce72cdb8a922f30ec09efa7cbfefbf5bfc
Joeries Young <joeries.young@gmail.com>
2025-05-31 14:58:42 +0800

    Update BLE demo.
    Update BLE demo.

---
+++ testing-stuff/bluetooth/BLEHelper/BLEHelper.Python.Invoker/BluetoothLE.cs
@@ -0,0 +1,207 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Read data from BLE (Bluetooth Low Energy) devices using Python scripts.
+/// </summary>
+public static class BluetoothLE
+{
+    /// <summary>
+    /// DataChangedEventArgs is used to pass data when a characteristic changes.
+    /// </summary>
+    public class DataChangedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The UUID of the characteristic that has changed.
+        /// </summary>
+        public string CharacteristicUUID { get; set; }
+        /// <summary>
+        /// The value of the characteristic that has changed, decoded as a UTF-8 string or a space-separated HEX string.
+        /// </summary>
+        public string Value { get; set; }
+
+        public DataChangedEventArgs(string characteristicUUID, string value)
+        {
+            CharacteristicUUID = characteristicUUID;
+            Value = value;
+        }
+    }
+
+    /// <summary>
+    /// Delegate for handling data change events.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public delegate void DataChangedHandler(object sender, DataChangedEventArgs e);
+
+    /// <summary>
+    /// Event that is raised when data changes in a BLE characteristic.
+    /// </summary>
+    public static event DataChangedHandler OnDataChanged;
+
+    /// <summary>
+    /// Regex to match the output from the Python script that indicates data has changed.
+    /// Format:
+    ///     Data Changed: {CharacteristicUUID} = {Value}
+    ///     Value is decoded as a UTF-8 string or a space-separated HEX string.
+    /// Example:
+    ///     Data Changed: 85bc9e6c-9501-4bf4-819e-4f40b5e56372 = force: 83.66
+    ///     Data Changed: 85bc9e6c-9501-4bf4-819e-4f40b5e56372 = A0 78 D5 90
+    /// </summary>
+    private static readonly Regex regexData = new Regex(@"^Data Changed: ([\dA-Za-z\-]+) = (.+)$");
+
+    /// <summary>
+    /// CancellationTokenSource is used to cancel the operation of reading data from BLE devices.
+    /// </summary>
+    private static CancellationTokenSource cts;
+
+    /// <summary>
+    /// pythonPath is the path to the Python.
+    /// </summary>
+    private static readonly string pythonPath;
+
+    /// <summary>
+    /// pythonScriptPath is the path to the Python script that will be executed.
+    /// </summary>
+    private static readonly string pythonScriptPath;
+
+    /// <summary>
+    /// processStartInfo is used to configure the process that runs the Python script.
+    /// </summary>
+    private static readonly ProcessStartInfo processStartInfo;
+
+    /// <summary>
+    /// pythonProcess is the process that runs the Python script.
+    /// </summary>
+    private static Process pythonProcess;
+
+    /// <summary>
+    /// Set pythonFilePath and processStartInfo based on the operating system.
+    /// </summary>
+    static BluetoothLE()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            pythonPath = "python";
+            pythonScriptPath = "..\\..\\..\\..\\BLEHelper.Python\\runner-win.bat";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            pythonPath = "python3";
+            pythonScriptPath = "..\\..\\..\\..\\BLEHelper.Python\\runner-linux.sh";
+        }
+        else
+        {
+            pythonPath = "python3";
+            pythonScriptPath = "..\\..\\..\\..\\BLEHelper.Python\\runner-mac.sh";
+        }
+
+        processStartInfo = new ProcessStartInfo
+        {
+            FileName = pythonScriptPath,
+            Arguments = "",
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            StandardInputEncoding = System.Text.Encoding.UTF8,
+            StandardOutputEncoding = System.Text.Encoding.UTF8,
+            StandardErrorEncoding = System.Text.Encoding.UTF8
+        };
+    }
+
+    /// <summary>
+    /// Start the Python script to read data from BLE devices.
+    /// </summary>
+    public static bool Start()
+    {
+        Stop();
+
+        cts = new CancellationTokenSource();
+        var task = System.Threading.Tasks.Task.Run(async () =>
+        {
+            try
+            {
+                processStartInfo.Arguments = pythonPath;
+                using (pythonProcess = Process.Start(processStartInfo))
+                {
+                    if (null == pythonProcess)
+                    {
+                        return;
+                    }
+                    pythonProcess.EnableRaisingEvents = true;
+                    pythonProcess.BeginErrorReadLine();
+                    pythonProcess.BeginOutputReadLine();
+                    pythonProcess.ErrorDataReceived += (sender, e) =>
+                    {
+                        if (!string.IsNullOrEmpty(e.Data))
+                        {
+                            Console.WriteLine($"[BluetoothLE] {e.Data}");
+                        }
+                    };
+                    pythonProcess.OutputDataReceived += (sender, e) =>
+                    {
+                        File.AppendAllText("D:\\ble.txt", e.Data + "\n");
+                        if (!string.IsNullOrEmpty(e.Data))
+                        {
+                            if (!e.Data.StartsWith("Device Scanned: ") &&
+                                !e.Data.StartsWith("Device Connected: ") &&
+                                !e.Data.StartsWith("Data Changed: "))
+                            {
+                                Console.WriteLine($"[BluetoothLE] {e.Data}");
+                            }
+                            var m = regexData.Match(e.Data);
+                            if (m.Success)
+                            {
+                                OnDataChanged?.Invoke(null, new DataChangedEventArgs(
+                                    m.Groups[1].Value,
+                                    m.Groups[2].Value));
+                            }
+                        }
+                    };
+                    await pythonProcess.WaitForExitAsync(cts.Token);
+                    pythonProcess.EnableRaisingEvents = false;
+                    pythonProcess.CancelOutputRead();
+                    pythonProcess.CancelErrorRead();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to Start BluetoothLE.\n{0}", ex.ToString());
+                throw;
+            }
+        }, cts.Token);
+        return !task.IsFaulted;
+    }
+
+    /// <summary>
+    /// Stop the Python script that is reading data from BLE devices.
+    /// </summary>
+    public static void Stop()
+    {
+        if (null != cts)
+        {
+            cts.Cancel();
+            cts = null;
+        }
+
+        if (pythonProcess != null)
+        {
+            pythonProcess.EnableRaisingEvents = false;
+            try
+            {
+                if (!pythonProcess.HasExited)
+                {
+                    pythonProcess.CancelOutputRead();
+                    pythonProcess.CancelErrorRead();
+                    pythonProcess.Kill();
+                }
+            }
+            catch { }
+            pythonProcess.Dispose();
+            pythonProcess = null;
+        }
+    }
+}
--- testing-stuff/bluetooth/BLEHelper/BLEHelper.Python.Invoker/Program.cs
+++ testing-stuff/bluetooth/BLEHelper/BLEHelper.Python.Invoker/Program.cs
@@ -8,76 +8,24 @@ namespace BLEHelper.Python.Invoker
     {
         static void Main(string[] args)
         {
-            var regexData = new Regex(@"^Data Changed: [\dA-Za-z\-]+ = (.+)$");
+            BluetoothLE.OnDataChanged += BluetoothLE_OnDataChanged; //Subscribe BluetoothLE data changed event anywhere else that needs to read BluetoothLE data.
+            BluetoothLE.Start();//Start BluetoothLE before the main window is created or anywhere else that needs BluetoothLE.
 
-            var fileName = string.Empty;
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                fileName = "..\\..\\..\\..\\BLEHelper.Python\\runner-win.bat";
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                fileName = "..\\..\\..\\..\\BLEHelper.Python\\runner-linux.sh";
-            }
-            else
-            {
-                fileName = "..\\..\\..\\..\\BLEHelper.Python\\runner-mac.sh";
-            }
+            Console.WriteLine("Press any key to stop BluetoothLE and exit the application...");
+            Console.Read();
 
-            ProcessStartInfo processStartInfo = new ProcessStartInfo
-            {
-                FileName = fileName,
-                Arguments = "",
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                StandardInputEncoding = System.Text.Encoding.UTF8,
-                StandardOutputEncoding = System.Text.Encoding.UTF8,
-                StandardErrorEncoding = System.Text.Encoding.UTF8
-            };
-            try
-            {
-                using (var process = Process.Start(processStartInfo))
-                {
-                    if (null == process)
-                    {
-                        return;
-                    }
-                    process.EnableRaisingEvents = true;
-                    process.BeginErrorReadLine();
-                    process.BeginOutputReadLine();
-                    process.ErrorDataReceived += (sender, e) =>
-                    {
-                        if (!string.IsNullOrEmpty(e.Data))
-                        {
-                            //Console.WriteLine($"{e.Data}");
-                        }
-                    };
-                    process.OutputDataReceived += (sender, e) =>
-                    {
-                        if (!string.IsNullOrEmpty(e.Data))
-                        {
-                            //Console.WriteLine(e.Data);
-                            var m = regexData.Match(e.Data);
-                            if (m.Success)
-                            {
-                                Console.WriteLine(m.Groups[1].Value);
-                            }
-                        }
-                    };
-                    process.WaitForExit();
-                    process.CancelOutputRead();
-                    process.CancelErrorRead();
-                    process.EnableRaisingEvents = false;
-                }
-            }
-            catch (Exception ex)
-            {
-                //Console.WriteLine($"Error Occurred: {ex.Message}");
-                return;
-            }
+            BluetoothLE.OnDataChanged -= BluetoothLE_OnDataChanged; //Unsubscribe BluetoothLE data changed event anywhere else that doesn't need to read BluetoothLE data any more.
+            BluetoothLE.Stop();//Stop BluetoothLE after the main window is destroyed or anywhere else that doesn't need BluetoothLE any more.
+        }
+
+        /// <summary>
+        /// BluetoothLE data changed event handler.
+        /// </summary>
+        /// <param name="sender">Always null</param>
+        /// <param name="e">DataChangedEventArgs</param>
+        private static void BluetoothLE_OnDataChanged(object sender, BluetoothLE.DataChangedEventArgs e)
+        {
+            Console.WriteLine(e.Value);
         }
     }
 }

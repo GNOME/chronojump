29576fdc5897d0ea05a950bfd541cf33a396453a
Xavier de Blas <xaviblas@gmail.com>
2025-06-17 16:19:56 +0200

    chronojump_importer.py working with fourPlatforms

--- src/chronojump-importer/chronojump_importer.py
+++ src/chronojump-importer/chronojump_importer.py
@@ -41,7 +41,7 @@ g_destinationBaseDirectory = ""
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Copyright (C) 2016-2017 Carles Pina i Estany <carles@pina.cat>
- * Copyright (C) 2019-2023 Xavier de Blas <xaviblas@gmail.com>
+ * Copyright (C) 2019-2025 Xavier de Blas <xaviblas@gmail.com>
  */
 """
 
@@ -494,6 +494,7 @@ class ImportSession:
         self._import_runs()
         self._import_pulse()
         self._import_wilight()
+        self._import_fourPlatforms()
         trigger = self._import_encoder()
         triggerForceSensor = self._import_forceSensor()
         triggerRunEncoder = self._import_runEncoder()
@@ -674,6 +675,16 @@ class ImportSession:
         wilight.update_ids("personID", self.persons77, "uniqueID", "new_uniqueID")
         wilight.update_session_ids(self.new_session_id)
         self.destination_db.write(wilight, self.destination_db.column_names("Wilight", skip_columns=["uniqueID", "personID"]))
+
+    def _import_fourPlatforms(self):
+        #self._print_status(self, "fourPlatforms")
+
+        # Imports FourPlatforms table
+        fourPlatforms = self.source_db.read(table_name="FourPlatforms",
+                                    where_condition="FourPlatforms.sessionID={}".format(self.source_session))
+        fourPlatforms.update_ids("personID", self.persons77, "uniqueID", "new_uniqueID")
+        fourPlatforms.update_session_ids(self.new_session_id)
+        self.destination_db.write(fourPlatforms, self.destination_db.column_names("FourPlatforms", skip_columns=["uniqueID", "personID"]))
 
     def _import_person_session77(self):
         # Imports PersonSession77

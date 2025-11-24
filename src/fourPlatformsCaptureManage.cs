a97b6050d9be4a6d2e0b7136aba0dffacc54b7b7
Xavier de Blas <xaviblas@gmail.com>
2025-11-24 12:44:39 +0100

    Doing #1160 now just need to take care double contacts same platform

--- src/fourPlatformsCaptureManage.cs
+++ src/fourPlatformsCaptureManage.cs
@@ -337,13 +337,18 @@ public class FourPlatformsCaptureManageSteps
 	public int StepsCompleted { get { return stepsCompleted; } }
 }
 
-
 public class FourPlatformsCaptureManageStepsLowHigh : FourPlatformsCaptureManageSteps
 {
 	List<PointF> on_l; // when landed: all contacts
 	List<PointF> off_l; // when lift off: all lifts
 
-	//List<PointF> onLast4Relevant_l; // when landed (if 1, 2a, 2b, 3a, 3b, 4) a, b are two contacts on same platform, it will take just first one
+	List<PointF> on3_l; // when landed: contact 3
+	List<PointF> on4_l; // when landed: contact 4
+	List<PointF> off1_l; // when leave: contact 1
+	List<PointF> off2_l; // when leave: contact 2
+
+	private bool lastContactIs3;
+	private bool firstOffIs1;
 
 	// constructor
 	public FourPlatformsCaptureManageStepsLowHigh (
@@ -351,115 +356,166 @@ public class FourPlatformsCaptureManageStepsLowHigh : FourPlatformsCaptureManage
 			ref List<PointF> stepsBottom_l, ref List<PointF> stepsTop_l)
 	{
 		init (captureType, stepsTotal, stepsBottom_l, stepsTop_l);
-
-		on_l = new List<PointF> ();
-		off_l = new List<PointF> ();
-		//onLast4Relevant_l = new List<PointF> ();
+		resetLists ();
 	}
 
-	public override void UpdateSteps (FourPlatformsEvent fpe, List<double> timeAccu_l, int y)
+	public override void UpdateSteps (FourPlatformsEvent fpe, List<double> timeAccu_l, int y) //y is 1-4
 	{
-		//LogB.Information ("timeAccu_l:");
-		//LogB.Information (UtilList.ListDoubleToString (timeAccu_l, 2, " "));
+		LogB.Information ("timeAccu_l:");
+		LogB.Information (UtilList.ListDoubleToString (timeAccu_l, 2, " "));
+		LogB.Information ("fpe:");
+		LogB.Information (fpe.ToString ());
 
 		// 1 update lists
+		bool processSteps = false; // if not process, just return
 		if (fpe.Time > 0) // is contact (in positive we have time flying)
+		{
 			on_l.Add (new PointF (timeAccu_l[fpe.Button], y));
-		else
+
+			if (fpe.Button == 2)
+			{
+				//LogB.Information ("3 on");
+				on3_l.Add (new PointF (timeAccu_l[2], y));
+				processSteps = true;
+			}
+			else if (fpe.Button == 3)
+			{
+				//LogB.Information ("4 on");
+				on4_l.Add (new PointF (timeAccu_l[3], y));
+				processSteps = true;
+			}
+		}
+		else // is leaving
+		{
 			off_l.Add (new PointF (timeAccu_l[fpe.Button], y));
 
-		// 2 exit if less than 4 contacts
-		if (on_l.Count < 4)
-			return;
-
-		// 3 create onLast4Relevant_l list omitting same sensor consecutive repeated values
-		List<PointF> onLast4Relevant_l = createLast4RelevantList ();
-
-		// 2 exit if less than 4 relevant contacts
-		if (onLast4Relevant_l.Count < 4)
-			return;
-
-		//TODO: continue
-		// 3 exit if we cannot find the flight after the onLast4
-		PointF ol4go = getStepStartOff (onLast4Relevant_l[3]);
-		if (ol4go.Y < 0)
-			return;
-
-		// 4 check if step is completed
-		if (
-				onLast4Relevant_l[0].Y >= 3 && onLast4Relevant_l[1].Y >= 3 && 	// Last two contacts are 3 or 4
-				onLast4Relevant_l[0].Y != onLast4Relevant_l[1].Y &&		// Last two contacts are different
-				onLast4Relevant_l[2].Y <= 2 && onLast4Relevant_l[3].Y <= 2 &&
-				onLast4Relevant_l[2].Y != onLast4Relevant_l[3].Y)
-		{
-			stepsBottom_l.Add (ol4go);
-			stepsTop_l.Add (onLast4Relevant_l[0]);
-
-			if (stepsCompleted == 0)
-				timeStart = ol4go.X;
-
-			stepsCompleted ++;
-
-			if (stepsCompleted >= stepsTotal)
-				timeEnd = onLast4Relevant_l[0].X;
-
-			// reset on_l, off_l to not count again on next contact
-			on_l = new List<PointF> ();
-			off_l = new List<PointF> ();
-			//onLast4Relevant_l = new List<PointF> ();
+			if (fpe.Button == 0)
+			{
+				//LogB.Information ("1 off");
+				off1_l.Add (new PointF (timeAccu_l[0], y));
+			}
+			else if (fpe.Button == 1)
+			{
+				//LogB.Information ("2 off");
+				off2_l.Add (new PointF (timeAccu_l[1], y));
+			}
 		}
+
+		if (! processSteps)
+			return;
+
+		LogB.Information ("processSteps ok");
+		// 2 exit if any of the lists is empty
+		if (on3_l.Count == 0 || on4_l.Count == 0 || off1_l.Count == 0 || off2_l.Count == 0)
+			return;
+
+		LogB.Information ("4 lists not empty");
+		// if off on 1 and 2 are more on the right than 3 on or 4 on (strange, but ...) return
+		if (PointF.Last (off1_l).X > PointF.Last (on3_l).X && PointF.Last (off1_l).X > PointF.Last (on4_l).X)
+			return;
+		if (PointF.Last (off2_l).X > PointF.Last (on3_l).X && PointF.Last (off2_l).X > PointF.Last (on4_l).X)
+			return;
+
+		LogB.Information ("no off more at right than ons");
+		// 3 create onLast4Relevant_l list omitting same sensor consecutive repeated values
+		List<PointF> last4Relevant_l = createLast4RelevantList ();
+
+		if (existsAn12ContactAfter3or4Flight (last4Relevant_l))
+			return;
+		
+		LogB.Information ("no exists an 1,2 contact after 3 or 4 flight");
+
+		LogB.Information ("success! Saving step");
+		stepsBottom_l.Add (last4Relevant_l[3]);
+		stepsTop_l.Add (last4Relevant_l[0]);
+	
+		if (stepsCompleted == 0)
+			timeStart = last4Relevant_l[3].X;
+
+		stepsCompleted ++;
+
+		if (stepsCompleted >= stepsTotal)
+			timeEnd = last4Relevant_l[0].X;
+
+		resetLists (); // reset lists to not count again on next contact
 	}
 
-	// omit same sensor consecutive repeated values
-	// if the penultimate or antepenultimate have duplicated contacts (with same sensor) take the first one
-	// and manage correctly the pos for the next relavant contacts
+	// check last two ON on 3, 4
+	// check last two OFF on 1, 2
+	// there should be an ON on absolute right
+	// there should be an OFF on absolute left
+	// TODO:
+	// 	if repeated on the same channel, on ON take the left
+	// 	if repeated on the same channel, on OFF take the right
+
+	private void resetLists ()
+	{
+		on_l = new List<PointF> ();
+		off_l = new List<PointF> ();
+
+		on3_l = new List<PointF> ();
+		on4_l = new List<PointF> ();
+		off1_l = new List<PointF> ();
+		off2_l = new List<PointF> ();
+	}
+
 	private List<PointF> createLast4RelevantList ()
 	{
-		List<PointF> l = new List<PointF> ();
+		List<PointF> l = new List<PointF> (); //from newest (right) to oldest (left)
 
-		// last (take current)
-		int pos = on_l.Count -1;
-		l.Add (on_l[pos]);
+		lastContactIs3 = false;
+		if (PointF.Last (on3_l).X > PointF.Last (on4_l).X)
+			lastContactIs3 = true;
+		
+		firstOffIs1 = false;
+		if (PointF.Last (off1_l).X < PointF.Last (off2_l).X)
+			firstOffIs1 = true;
 
-		// penultimate
-		pos --;
-		int found = pos;
-		for (int i = pos-1; i >= 0 && on_l[i].Y == on_l[pos].Y; i --)
-			found --;
-
-		if (found < 0)
-			return l;
-
-		pos = found;
-		l.Add (on_l[pos]);
-
-		// antepenultimate
-		pos --;
-		found = pos;
-		for (int i = pos-1; i >= 0 && on_l[i].Y == on_l[pos].Y; i --)
-			found --;
-
-		if (found < 0)
-			return l;
-
-		pos = found;
-		l.Add (on_l[pos]);
-
-		// first (take always the more at right)
-		pos --;
-		if (pos < 0)
-			return l;
-
-		l.Add (on_l[pos]);
+		if (lastContactIs3) //TODO: add this code above
+		{
+			l.Add (PointF.Last (on3_l));
+			l.Add (PointF.Last (on4_l));
+		} else {
+			l.Add (PointF.Last (on4_l));
+			l.Add (PointF.Last (on3_l));
+		}
+			
+		if (firstOffIs1) //TODO: add this code above
+		{
+			l.Add (PointF.Last (off2_l));
+			l.Add (PointF.Last (off1_l));
+		} else {
+			l.Add (PointF.Last (off1_l));
+			l.Add (PointF.Last (off2_l));
+		}
 		return l;
 	}
 
-	private PointF getStepStartOff (PointF stepStartContact)
+	private bool existsAn12ContactAfter3or4Flight (List<PointF> last4Relevant_l)
 	{
-		foreach (PointF p in off_l)
-			if (p.X > stepStartContact.X && p.Y == stepStartContact.Y)
-				return p;
+		LogB.Information ("at existsAn12...");
+		int previousTopSensor = 3; // 1-4
+		if (lastContactIs3)
+			previousTopSensor = 4; // 1-4
 
-		return new PointF (-1, -1);
+		// check if previousTopSensor has lift and after there is a contact on 1 or 2
+		double previousTopOffX = -1;
+		foreach (PointF p in off_l)
+		{
+			//LogB.Information ("p: " + p.ToString ());
+			if (p.Y == previousTopSensor &&
+					p.X > last4Relevant_l[1].X && //this off has to be at right of penultimate (note last is 0)
+					p.X > previousTopOffX)
+				previousTopOffX = p.X;
+		}
+
+		if (previousTopOffX < 0)
+			return false;
+
+		foreach (PointF p in off_l)
+			if (p.Y <= 2 && p.X >= previousTopOffX)
+				return true;
+
+		return false;
 	}
 }
